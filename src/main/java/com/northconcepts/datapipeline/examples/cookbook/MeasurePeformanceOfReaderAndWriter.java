bc21832a8b7830e921423ee26a8e71b6c4ded724
Joshi Gautam <gvjoshi25@gmail.com>
2020-09-11 18:50:50 +0530

    updated example to measure performance of reader & writer

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/MeasurePeformanceOfReaderAndWriter.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/MeasurePeformanceOfReaderAndWriter.java
@@ -11,15 +11,14 @@ import java.io.File;
 import com.northconcepts.datapipeline.core.DataReader;
 import com.northconcepts.datapipeline.core.DataWriter;
 import com.northconcepts.datapipeline.core.Endpoint;
-import com.northconcepts.datapipeline.core.NullWriter;
 import com.northconcepts.datapipeline.csv.CSVReader;
 import com.northconcepts.datapipeline.csv.CSVWriter;
-import com.northconcepts.datapipeline.examples.cookbook.common.card.CardReader;
 import com.northconcepts.datapipeline.job.Job;
 import com.northconcepts.datapipeline.transform.BasicFieldTransformer;
 import com.northconcepts.datapipeline.transform.CopyField;
 import com.northconcepts.datapipeline.transform.RenameField;
 import com.northconcepts.datapipeline.transform.TransformingReader;
+import com.northconcepts.datapipeline.transform.TransformingWriter;
 
 public class MeasurePeformanceOfReaderAndWriter {
 
@@ -27,11 +26,6 @@ public class MeasurePeformanceOfReaderAndWriter {
 		//set flag - captureElapsedTime to measure performance.
 		Endpoint.setCaptureElapsedTime(true);
 		
-		measurePerformanceOfReader();
-		measurePerformanceOfWriter();
-	}
-
-	private static void measurePerformanceOfReader() {
 		DataReader reader = new CSVReader(new File("example/data/input/trades.csv"))
 				.setFieldNamesInFirstRow(true);
 		
@@ -39,34 +33,23 @@ public class MeasurePeformanceOfReaderAndWriter {
 				.add(new BasicFieldTransformer("price").stringToDouble())
 				.add(new BasicFieldTransformer("time").stringToTime("hh:mm:s"));
 
-		Job job = Job.run(reader, new NullWriter());
-
-		System.out.println("Job Running Time:- " + job.getRunningTimeAsString());
-		System.out.println("Total Records read:- " + job.getRecordsTransferred());
-		
-		System.out.println("Time taken by TranformingReader:- " + reader.getSelfTimeAsString());
-		System.out.println("Time taken by CSVReader:- " + reader.getNestedEndpoint().getSelfTimeAsString());
-	}
-
-	private static void measurePerformanceOfWriter() {
-		//Create a CardReader to retrieve records.
-		DataReader reader = new CardReader()
-				.setMaxRecords(1000000)
-				.setStartingDate("2016-01-14 09:00:00.000")
-				.setDateIncrement(500);
-		
 		DataWriter writer = new CSVWriter(new File("example/data/output/writer_performance_measurement.csv"));
-		reader = new TransformingReader(reader)
-				.add(new CopyField("card_value", "card_value2"))
-				.add(new RenameField("card_number", "card_no"));
+
+		writer = new TransformingWriter(writer)
+				.add(new CopyField("stock", "stock_name"))
+				.add(new RenameField("shares", "share_count"));
 		
 		Job job = Job.run(reader, writer);
 
 		System.out.println("Job Running Time:- " + job.getRunningTimeAsString());
-		System.out.println("Total Records:- " + job.getRecordsTransferred());
+		System.out.println("Total Records transferred:- " + job.getRecordsTransferred());
 		
 		System.out.println("Time taken by TranformingReader:- " + reader.getSelfTimeAsString());
-		System.out.println("Time taken by CSVWriter:- " + reader.getNestedEndpoint().getSelfTimeAsString());
-
+		System.out.println("Time taken by CSVReader:- " + reader.getNestedEndpoint().getSelfTimeAsString());
+		
+		System.out.println("Time taken by TransformingWriter:- " + writer.getSelfTimeAsString());
+		System.out.println("Time taken by CSVWriter:- " + writer.getNestedEndpoint().getSelfTimeAsString());
+		
 	}
+	
 }
