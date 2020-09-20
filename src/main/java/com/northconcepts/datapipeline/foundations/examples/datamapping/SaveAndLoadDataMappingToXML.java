5cbfe6c6dfe6f0447280898af46bf21606555726
Gautam Joshi <gvjoshi25@gmail.com>
2020-09-20 20:49:12 +0530

    DP-1003 - Add supplementary DPF examples for 6.0 (#20)
    * DP-1003 - Add supplementary DPF examples for 6.0
    
    * Fixed file name
    
    * Fixed Input, output directories path
    
    * Adding license to java files

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/datamapping/SaveAndLoadDataMappingToXML.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2006-2020 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.foundations.examples.datamapping;
+
+import java.io.FileInputStream;
+import java.io.FileWriter;
+
+import com.northconcepts.datapipeline.core.FieldType;
+import com.northconcepts.datapipeline.foundations.datamapping.DataMapping;
+import com.northconcepts.datapipeline.foundations.datamapping.FieldMapping;
+import com.northconcepts.datapipeline.foundations.schema.BooleanFieldDef;
+import com.northconcepts.datapipeline.foundations.schema.EntityDef;
+import com.northconcepts.datapipeline.foundations.schema.TextFieldDef;
+
+public class SaveAndLoadDataMappingToXML {
+
+    public static void main(String[] args) throws Throwable {
+
+        // Save DataMapping to XML file.
+        DataMapping mapping = new DataMapping()
+                .setName("Customer Details")
+                .addFieldMapping(new FieldMapping("owner", "${source.customerid}").setCondition("${source.customerid} == 10001"))
+                .addFieldMapping(new FieldMapping("first_name", "${source.customer_fname}"))
+                .addFieldMapping(new FieldMapping("last_name", "${source.customer_lname}"))
+                .addFieldMapping(new FieldMapping("name", "${target.first_name} + ' ' + ${target.last_name}"))
+                .addFieldMapping(new FieldMapping("phone", "${source.customer-contact}"))
+                .addFieldMapping(new FieldMapping("email", "${source.customer-email}"))
+                .addFieldMapping(new FieldMapping("isActive", "${source.customer-active}"));
+
+        EntityDef entityDef = new EntityDef()
+                .setName("Order Details")
+                .addField(new TextFieldDef("OrderName", FieldType.STRING)
+                            .setRequired(true)
+                            .setMinimumLength(0)
+                            .setMaximumLength(2))
+                .addField(new BooleanFieldDef("isActiveOrder", FieldType.BOOLEAN));
+
+        mapping.setEntity(entityDef);
+
+        mapping.toXml(new FileWriter("example/data/output/customer-details-data-mapping.xml"), true);
+        
+        //Load DataMapping from this XML File.
+        DataMapping mappingFromXml = new DataMapping()
+                .fromXml(new FileInputStream("example/data/output/customer-details-data-mapping.xml"));
+        
+        System.out.println("DataMapping loaded from XML:-\n" + mappingFromXml);
+    }
+
+}
---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/pipeline/LoadSnapshotOfDataset.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2006-2020 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.foundations.examples.pipeline;
+
+import java.util.concurrent.TimeUnit;
+
+import com.northconcepts.datapipeline.foundations.file.LocalFile;
+import com.northconcepts.datapipeline.foundations.pipeline.Pipeline;
+import com.northconcepts.datapipeline.foundations.pipeline.dataset.Dataset;
+import com.northconcepts.datapipeline.foundations.pipeline.input.CsvPipelineInput;
+
+public class LoadSnapshotOfDataset {
+
+    public static void main(String[] args) {
+        Pipeline pipeline = new Pipeline();
+
+        CsvPipelineInput pipelineInput = new CsvPipelineInput()
+                .setFileSource(new LocalFile().setPath("example/data/input/trades.csv"))
+                .setFieldNamesInFirstRow(true);
+
+        pipeline.setInput(pipelineInput);
+        
+        Dataset dataset = pipeline.getDataset();
+        dataset.load().waitForRecordsToLoad(10000, TimeUnit.SECONDS.toMillis(4));
+
+        System.out.println("Total Records:- " + dataset.getRecordCount());
+    }
+}
