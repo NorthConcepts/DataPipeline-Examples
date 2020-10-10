1e7889772b479670dbbe220cc8410fc189b4e38f
Dele Taylor <dele.taylor@northconcepts.com>
2020-10-10 04:46:47 -0400

    Update formatting.

--- src/main/java/com/northconcepts/datapipeline/foundations/examples/schema/UseSchemaFilterToValidateRecordsInAPipeline.java
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/schema/UseSchemaFilterToValidateRecordsInAPipeline.java
@@ -21,33 +21,28 @@ import com.northconcepts.datapipeline.validate.ValidatingReader;
 public class UseSchemaFilterToValidateRecordsInAPipeline {
 
     public static void main(String[] args) {
-        
+
         EntityDef entityDef = new EntityDef().setName("Jewelry")
-                .addField(new NumericFieldDef("Variant Price", FieldType.DOUBLE)
-                        .setMaximum(5)
-                        .setMaximum(500)
-                        .setRequired(true))
-                .addField(new BooleanFieldDef("Variant Taxable", FieldType.BOOLEAN))
-                .addField(new TextFieldDef("Title", FieldType.STRING)
-                        .setMaximumLength(256))
-                .addField(new TextFieldDef("Option1 Value", FieldType.STRING));
-        
-        SchemaFilter schemaFilter = new SchemaFilter(entityDef);
-                
+            .addField(new NumericFieldDef("Variant Price", FieldType.DOUBLE).setMaximum(5).setMaximum(500).setRequired(true))
+            .addField(new BooleanFieldDef("Variant Taxable", FieldType.BOOLEAN))
+            .addField(new TextFieldDef("Title", FieldType.STRING).setMaximumLength(256))
+            .addField(new TextFieldDef("Option1 Value", FieldType.STRING));
+
         DataReader reader = new CSVReader(new File("data/input/jewelry.csv"))
-                .setAllowMultiLineText(true)
-                .setFieldNamesInFirstRow(true);
-        
+            .setAllowMultiLineText(true)
+            .setFieldNamesInFirstRow(true);
+
         reader = new TransformingReader(reader)
-                .add(new BasicFieldTransformer("Variant Price").nullToValue(100d).stringToDouble(),
-                    new BasicFieldTransformer("Variant Taxable").stringToBoolean())
-                .add(new SelectFields("Title", "Variant Price", "Variant Taxable", "Option1 Value"));
-        
-        reader = new ValidatingReader(reader).add(schemaFilter);
-        
+            .add(
+                new BasicFieldTransformer("Variant Price").nullToValue(100d).stringToDouble(), 
+                new BasicFieldTransformer("Variant Taxable").stringToBoolean())
+            .add(new SelectFields("Title", "Variant Price", "Variant Taxable", "Option1 Value"));
+
+        reader = new ValidatingReader(reader).add(new SchemaFilter(entityDef));
+
         DataWriter writer = StreamWriter.newSystemOutWriter();
-        
+
         Job.run(reader, writer);
     }
-    
+
 }
