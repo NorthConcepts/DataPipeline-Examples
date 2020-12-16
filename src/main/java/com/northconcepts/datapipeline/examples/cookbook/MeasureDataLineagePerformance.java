0ee828c2a7c1a84b892adbc4025ce8b9c7b5e1e5
Dele Taylor <dele.taylor@northconcepts.com>
2020-12-16 07:41:16 -0500

    DP-1314 Add short JVM warmup. Refactor code to run all tests at once.

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/MeasureDataLineagePerformance.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/MeasureDataLineagePerformance.java
@@ -15,24 +15,37 @@ import com.northconcepts.datapipeline.job.Job;
 
 public class MeasureDataLineagePerformance {
 
-    private static final boolean saveLineage = true;
-    
     public static void main(String[] args) {
-        readCsv("1000_Sales_Records.csv");
-        readCsv("100000_Sales_Records.csv");
+        warmupJvm();
+
+        readCsv("1000_Sales_Records.csv", false);
+        readCsv("1000_Sales_Records.csv", true);
+        readCsv("100000_Sales_Records.csv", false);
+        readCsv("100000_Sales_Records.csv", true);
     }
 
-    private static void readCsv(String fileName) {
+    private static void warmupJvm() {
+        System.out.println("Warming up JVM");
+        
+        for (int i = 0; i < 3; i++) {
+            DataReader reader = new CSVReader(new File("example/data/input/100000_Sales_Records.csv"))
+                    .setFieldNamesInFirstRow(true)
+                    .setSaveLineage(true);
+            Job.run(reader, new NullWriter());
+        }
+    }
+
+    private static void readCsv(String fileName, boolean saveLineage) {
         System.out.println("--------------------------------------------------------");
-        System.out.println("Lineage enabled: " + saveLineage);
         DataReader reader = new CSVReader(new File("example/data/input/" + fileName))
                 .setFieldNamesInFirstRow(true)
                 .setSaveLineage(saveLineage);
 
         Job job = Job.run(reader, new NullWriter());
 
-        System.out.println("Job Running Time:- " + job.getRunningTimeAsString());
-        System.out.println("Total Records transferred:- " + job.getRecordsTransferred());
+        System.out.println("Lineage enabled: " + saveLineage);
+        System.out.println("Records: " + job.getRecordsTransferred());
+        System.out.println("Time: " + job.getRunningTimeAsString());
     }
     
 }
