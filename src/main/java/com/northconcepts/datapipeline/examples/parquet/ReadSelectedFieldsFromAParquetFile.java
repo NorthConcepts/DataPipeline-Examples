6307ac7b04411730ae3f895d27829f17467457de
Dele Taylor <dele.taylor@northconcepts.com>
2021-02-10 00:17:24 -0500

    DP-1252 Add excluded fields in comments.

--- src/main/java/com/northconcepts/datapipeline/examples/parquet/ReadSelectedFieldsFromAParquetFile.java
+++ src/main/java/com/northconcepts/datapipeline/examples/parquet/ReadSelectedFieldsFromAParquetFile.java
@@ -18,16 +18,27 @@ import com.northconcepts.datapipeline.parquet.ParquetDataReader;
 public class ReadSelectedFieldsFromAParquetFile {
 
     public static void main(String[] args) {
-        // Prepare schema with specified fields and those fields will be read from file.
+        // Prepare schema with fields to be read from file, ignoring other fields
         MessageType schema = new MessageType("input_schema",
                 Types.optional(INT32).named("id"),
-                Types.optional(BOOLEAN).named("bool_col"),
                 Types.optional(INT32).named("int_col"),
                 Types.optional(DOUBLE).named("double_col"),
-                Types.optional(BINARY).as(stringType()).named("date_string_col"));
+                Types.optional(BINARY).as(stringType()).named("date_string_col"),
+                Types.optional(BOOLEAN).named("bool_col")
+                );
+/* 
+       Exclude the following fields from being read:
+            optional int32 tinyint_col;
+            optional int32 smallint_col;
+            optional int64 bigint_col;
+            optional float float_col;
+            optional binary string_col;
+            optional int96 timestamp_col;
+*/
         
-        ParquetDataReader reader = new ParquetDataReader(new File("example/data/input/read_parquet_file.parquet"));
-        reader.setSchema(schema); // provide input schema and read only selected fields
+        ParquetDataReader reader = new ParquetDataReader(new File("example/data/input/read_parquet_file.parquet"))
+                .setSchema(schema)  // remove this line to see all fields (and in original arrangement)
+                ;
         Job.run(reader, new StreamWriter(System.out));
 
         System.out.println(reader.getSchema());
