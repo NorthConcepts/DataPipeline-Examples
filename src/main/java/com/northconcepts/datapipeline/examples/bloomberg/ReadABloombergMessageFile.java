7fb8c72af488040f6fe59789ecf2c21005916dbc
Dele Taylor <dele.taylor@northconcepts.com>
2021-02-15 18:38:23 -0500

    DP-1260 add Bloomberg example

---
+++ src/main/java/com/northconcepts/datapipeline/examples/bloomberg/ReadABloombergMessageFile.java
@@ -0,0 +1,29 @@
+package com.northconcepts.datapipeline.examples.bloomberg;
+
+import java.io.File;
+
+import com.northconcepts.datapipeline.bloomberg.BloombergMessage;
+import com.northconcepts.datapipeline.bloomberg.BloombergMessageReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.job.Job;
+
+public class ReadABloombergMessageFile {
+
+    public static void main(String[] args) {
+        File file = new File("data/input/example-talend-stackoverflow.req");
+        
+        BloombergMessageReader reader = new BloombergMessageReader(file);
+        DataWriter writer = new StreamWriter(System.out);
+        Job.run(reader,  writer);
+        
+        // message is available after BloombergMessageReader.open() is called in Job.run(reader,  writer) above
+        BloombergMessage message = reader.getMessage();  
+        System.out.println("-----------------------------------------------");
+        System.out.println("Header: " + message.getHeader().getProperties());
+        System.out.println("Fields: " + message.getFields().getFields());
+        System.out.println("Records: " + message.getData().getRecords().size());
+        System.out.println("Footers: " + message.getFooter().getProperties());
+    }
+
+}
