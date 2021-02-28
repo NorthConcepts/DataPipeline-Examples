3eec7a80adccb593a3fe94a8602de0752798ef2b
Gautam Joshi <gvjoshi25@gmail.com>
2021-02-28 19:38:22 +0530

    DP-1430 - Create ParquetReader Lineage Example (#45)
    * DP-1430 - Create ParquetReader Lineage Example
    
    * Deleting UseDataLineage as it was renamed to UseDataLineageWithCsvReader

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/UseDataLineageWithParquetReader.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/UseDataLineageWithParquetReader.java
@@ -12,55 +12,50 @@ import com.northconcepts.datapipeline.core.DataReader;
 import com.northconcepts.datapipeline.core.DataWriter;
 import com.northconcepts.datapipeline.core.Field;
 import com.northconcepts.datapipeline.core.Record;
-import com.northconcepts.datapipeline.csv.CSVReader;
 import com.northconcepts.datapipeline.job.Job;
 import com.northconcepts.datapipeline.lineage.FieldLineage;
 import com.northconcepts.datapipeline.lineage.RecordLineage;
+import com.northconcepts.datapipeline.parquet.ParquetDataReader;
 
-public class UseDataLineage {
+public class UseDataLineageWithParquetReader {
 
     public static void main(String[] args) {
-        DataReader reader = new CSVReader(new File("example/data/input/rating-table-01.csv"))
-                .setFieldNamesInFirstRow(true)
-                .setSaveLineage(true); // Enable lineage support. (By default, it is disabled.) 
-                                          // If lineage is not supported by reader, exception will be thrown.
-        
+
+        DataReader reader = new ParquetDataReader(new File("example/data/input/read_parquet_file.parquet"))
+                                .setSaveLineage(true); // Enable lineage support. (By default, it is disabled.)
         Job.run(reader, new LineageWriter());
     }
-    
+
     public final static class LineageWriter extends DataWriter {
 
         @Override
         protected void writeImpl(Record record) throws Throwable {
             System.out.println(record);
-            
+
             RecordLineage recordLineage = new RecordLineage().setRecord(record);
-            
+
             System.out.println("Record Lineage");
             System.out.println("    File: " + recordLineage.getFile());
-            System.out.println("    File Line: " + recordLineage.getFileLineNumber());
-            System.out.println("    File Column: " + recordLineage.getFileColumnNumber());
             System.out.println("    Record: " + recordLineage.getRecordNumber());
-            
+            System.out.println("    Schema: " + recordLineage.getParquetSchema());
             System.out.println();
-            
+
             FieldLineage fieldLineage = new FieldLineage();
-            
+
             System.out.println("Field Lineage");
-            for (int i=0; i < record.getFieldCount(); i++) {
+            for (int i = 0; i < record.getFieldCount(); i++) {
                 Field field = record.getField(i);
                 fieldLineage.setField(field);
                 System.out.println("    " + field.getName());
                 System.out.println("        File: " + fieldLineage.getFile());
-                System.out.println("        File Line: " + fieldLineage.getFileLineNumber());
-                System.out.println("        File Column: " + fieldLineage.getFileColumnNumber());
                 System.out.println("        Record: " + fieldLineage.getRecordNumber());
                 System.out.println("        Field Index: " + fieldLineage.getOriginalFieldIndex());
                 System.out.println("        Field Name: " + fieldLineage.getOriginalFieldName());
+                System.out.println("        Field Schema: " + fieldLineage.getParquetFieldSchema());
             }
             System.out.println("---------------------------------------------------------");
             System.out.println();
         }
-        
+
     }
 }
