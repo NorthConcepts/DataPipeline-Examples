900f66dc4808452980d7b1fa7614297db7dccfd5
Bryan-Noel <noelbryansoftware@gmail.com>
2021-05-26 23:23:59 +0800

    DP-1503: init commit

---
+++ src/main/java/com/northconcepts/datapipeline/examples/twitter2/SearchTwitterForTweets.java
@@ -0,0 +1,23 @@
+package com.northconcepts.datapipeline.examples.twitter2;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.job.Job;
+
+
+public class SearchTwitterForTweets {
+
+    private static final String API_KEY = "YOUR_API_KEY";
+    private static final String API_SECRET = "YOUR_API_SECRET";
+    private static final String ACCESS_TOKEN = "YOUR_ACCESS_TOKEN";
+    private static final String ACCESS_TOKEN_SECRET = "YOUR_ACCESS_TOKEN_SECRET";
+    private static final String SEARCH_KEYWORD = "Corona";
+
+    public static void main(String[] args) {
+        DataReader reader = new TwitterSearchReader(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, SEARCH_KEYWORD);
+        DataWriter writer = new StreamWriter(System.out);
+
+        Job.run(reader, writer);
+    }
+}
