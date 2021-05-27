dcc23f883b5dac53270fda9846aa950a4ac93bcc
Bryan Noel <42918023+Bryan-Noel@users.noreply.github.com>
2021-05-28 00:25:51 +0800

    DP-1503-Move-TwitterV2-examples-to-Examples-repo (#56)
    * DP-1503: init commit
    
    * DP-1503-Move-TwitterV2-examples-to-Examples-repo
    
    * DP-1503 Undo change that forces downloading dependencies for every build
    
    Co-authored-by: Dele Taylor <dele.taylor@northconcepts.com>

---
+++ src/main/java/com/northconcepts/datapipeline/examples/twitter2/FilterTwitterInRealTime.java
@@ -0,0 +1,24 @@
+package com.northconcepts.datapipeline.examples.twitter2;
+
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.job.Job;
+import com.northconcepts.datapipeline.twitter2.TwitterFilterStreamReader;
+
+
+public class FilterTwitterInRealTime {
+
+    private static final String API_KEY = "YOUR_API_KEY";
+    private static final String API_SECRET = "YOUR_API_SECRET";
+
+    private static final String BEARER_TOKEN = "YOUR_BEARER_TOKEN";
+
+    public static void main(String[] args) {
+        TwitterFilterStreamReader reader = new TwitterFilterStreamReader(API_KEY, API_SECRET, BEARER_TOKEN);
+        reader.addRule("cat has:images", "cat images");
+
+        DataWriter writer = new StreamWriter(System.out);
+
+        Job.run(reader, writer);
+    }
+}
---
+++ src/main/java/com/northconcepts/datapipeline/examples/twitter2/SearchFollowersOfATwitterUser.java
@@ -0,0 +1,25 @@
+package com.northconcepts.datapipeline.examples.twitter2;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.job.Job;
+import com.northconcepts.datapipeline.twitter2.TwitterFollowerReader;
+
+
+public class SearchFollowersOfATwitterUser {
+
+    private static final String API_KEY = "YOUR_API_KEY";
+    private static final String API_SECRET = "YOUR_API_SECRET";
+    private static final String ACCESS_TOKEN = "YOUR_ACCESS_TOKEN";
+    private static final String ACCESS_TOKEN_SECRET = "YOUR_ACCESS_TOKEN_SECRET";
+
+    private static final String USERNAME = "TwitterDev";
+
+    public static void main(String[] args) {
+        DataReader reader = new TwitterFollowerReader(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, USERNAME);
+        DataWriter writer = new StreamWriter(System.out);
+
+        Job.run(reader, writer);
+    }
+}
