ffd1b4db95e58a799c0f39f89f778f54ba0f6001
Bryan-Noel <noelbryansoftware@gmail.com>
2021-05-28 19:36:30 +0800

    DP-1534-Add-position-to-declarative-datamapping-example

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/datamapping/DeclarativelyMapDataUsingPositions.java
@@ -0,0 +1,46 @@
+package com.northconcepts.datapipeline.foundations.examples.datamapping;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.core.NullWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.foundations.datamapping.DataMapping;
+import com.northconcepts.datapipeline.foundations.datamapping.DataMappingReader;
+import com.northconcepts.datapipeline.foundations.schema.EntityDef;
+import com.northconcepts.datapipeline.foundations.schema.SchemaDef;
+import com.northconcepts.datapipeline.foundations.schema.SchemaTransformer;
+import com.northconcepts.datapipeline.job.Job;
+import com.northconcepts.datapipeline.transform.TransformingReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class DeclarativelyMapDataUsingPositions {
+
+    public static void main(String... args) throws Throwable {
+        // Load source & target schema
+        SchemaDef schema = new SchemaDef()
+                .fromXml(new FileInputStream(new File("example/data/input/datamapping/account-schema-definition-position.xml")));
+        EntityDef sourceAccountEntity = schema.getEntity("SourceAccountEntity");
+
+        // Define job
+        DataReader reader = new CSVReader(new File("example/data/input/credit-balance-02-1000000.csv"))  // 1mm -> credit-balance-02-1000000.csv
+                .setFieldNamesInFirstRow(true);
+        
+        reader = new TransformingReader(reader)
+                .add(new SchemaTransformer(sourceAccountEntity));
+        
+//        reader = new AsyncReader(reader);  // Threading
+
+        DataMapping mapping = new DataMapping()
+                .fromXml(new FileInputStream("example/data/input/datamapping/credit-balance-mapping-2.xml"));
+        reader = new DataMappingReader(reader, mapping);
+
+        DataWriter writer = new NullWriter();
+//        DataWriter writer = StreamWriter.newSystemOutWriter();
+        
+        Job job = Job.run(reader, writer);
+        System.out.println("Records Transferred: " + job.getRecordsTransferred());
+        System.out.println("Running Time: " + job.getRunningTimeAsString());
+    }
+}
