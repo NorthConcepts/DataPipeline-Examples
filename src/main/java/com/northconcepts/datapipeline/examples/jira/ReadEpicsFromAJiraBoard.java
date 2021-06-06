c8f6f78a67c770b1a61b23a4beeb7777e41ac385
Bryan-Noel <noelbryansoftware@gmail.com>
2021-06-06 17:27:35 +0800

    DP-1548-Create-Jira-Examples

---
+++ src/main/java/com/northconcepts/datapipeline/examples/jira/ReadEpicsFromAJiraBoard.java
@@ -0,0 +1,20 @@
+package com.northconcepts.datapipeline.examples.jira;
+
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.jira.JiraEpicReader;
+import com.northconcepts.datapipeline.job.Job;
+
+public class ReadEpicsFromAJiraBoard {
+
+    private static final String JIRA_DOMAIN = "JIRA_DOMAIN";
+    private static final String JIRA_USERNAME = "USERNAME";
+    private static final String JIRA_API_KEY = "API_KEY";
+    private static final int JIRA_BOARD_ID = 4;
+
+    public static void main(String... args) {
+        JiraEpicReader reader = new JiraEpicReader(JIRA_DOMAIN, JIRA_USERNAME, JIRA_API_KEY, JIRA_BOARD_ID);
+        reader.setMaxItems(5);
+
+        Job.run(reader, new StreamWriter(System.out));
+    }
+}
---
+++ src/main/java/com/northconcepts/datapipeline/examples/jira/ReadJiraIssues.java
@@ -0,0 +1,23 @@
+package com.northconcepts.datapipeline.examples.jira;
+
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.jira.JiraIssueReader;
+import com.northconcepts.datapipeline.jira.JiraSearch;
+import com.northconcepts.datapipeline.job.Job;
+
+public class ReadJiraIssues {
+
+    private static final String JIRA_DOMAIN = "JIRA_DOMAIN";
+    private static final String JIRA_USERNAME = "USERNAME";
+    private static final String JIRA_API_KEY = "API_KEY";
+    private static final String JQL = "YOUR_JIRA_QUERY";
+
+    public static void main(String... args) {
+        JiraSearch jiraSearch = new JiraSearch()
+            .setJql(JQL)
+            .setMaxResults(5);
+
+        JiraIssueReader reader = new JiraIssueReader(JIRA_DOMAIN, JIRA_USERNAME, JIRA_API_KEY, jiraSearch);
+        Job.run(reader, new StreamWriter(System.out));
+    }
+}
