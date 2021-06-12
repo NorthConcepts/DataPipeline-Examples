638b30dd2e0882eead688f1be3534fdff286f875
Bryan-Noel <noelbryansoftware@gmail.com>
2021-06-12 08:34:15 +0800

    DP-1602: JSON example

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/pipeline/MapDataWithDataMappingPipelineFromJson.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2006-2021 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.foundations.examples.pipeline;
+
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.foundations.pipeline.DataMappingPipeline;
+import com.northconcepts.datapipeline.foundations.pipeline.output.DataWriterPipelineOutput;
+
+public class MapDataWithDataMappingPipelineFromJson {
+
+    public static void main(String[] args) throws Throwable {
+        String jsonString = new String(Files.readAllBytes(Paths.get("example/data/input/pipeline/datamappingpipeline.json")));
+        DataMappingPipeline pipeline = new DataMappingPipeline().fromJsonString(jsonString);
+        //pipeline.setOutput(new DataWriterPipelineOutput(() -> StreamWriter.newSystemOutWriter()));
+        pipeline.run();
+    }
+
+}
