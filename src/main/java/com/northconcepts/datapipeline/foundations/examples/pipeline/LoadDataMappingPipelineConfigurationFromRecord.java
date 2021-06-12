af65212b9b9ea58e8fc8d0171c66d933cdfaf7eb
Bryan-Noel <noelbryansoftware@gmail.com>
2021-06-13 07:22:11 +0800

    DP-1602: Java example

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/pipeline/LoadDataMappingPipelineConfigurationFromRecord.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2006-2021 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.foundations.examples.pipeline;
+
+import com.northconcepts.datapipeline.core.FieldType;
+import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.foundations.datamapping.DataMapping;
+import com.northconcepts.datapipeline.foundations.datamapping.FieldMapping;
+import com.northconcepts.datapipeline.foundations.file.LocalFile;
+import com.northconcepts.datapipeline.foundations.pipeline.DataMappingPipeline;
+import com.northconcepts.datapipeline.foundations.pipeline.input.CsvPipelineInput;
+import com.northconcepts.datapipeline.foundations.pipeline.output.ExcelPipelineOutput;
+import com.northconcepts.datapipeline.foundations.schema.EntityDef;
+import com.northconcepts.datapipeline.foundations.schema.NumericFieldDef;
+import com.northconcepts.datapipeline.foundations.schema.SchemaDef;
+import com.northconcepts.datapipeline.foundations.schema.TextFieldDef;
+
+public class LoadDataMappingPipelineConfigurationFromRecord {
+
+    public static void main(String[] args) {
+        SchemaDef schema = new SchemaDef()
+            .addEntity(new EntityDef().setName("Raw")
+                .addField(new TextFieldDef().setName("event_type").setRequired(true).setMaximumLength(25))
+                .addField(new TextFieldDef().setName("id").setRequired(true)))
+            .addEntity(new EntityDef().setName("Processed")
+                .addField(new TextFieldDef().setName("Event").setRequired(true).setMaximumLength(25))
+                .addField(new NumericFieldDef().setName("Call ID").setType(FieldType.INT).setRequired(true)));
+
+        DataMapping mapping = new DataMapping()
+                .addFieldMapping(new FieldMapping("Event", "source.event_type"))
+                .addFieldMapping(new FieldMapping("Call ID", "source.id"));
+
+        LocalFile source = new LocalFile().setPath("input/file/sample/path.csv");
+        LocalFile sink = new LocalFile().setPath("output/file/sample/path.csv");
+
+        //Create DataMappingPipeline with source and target entities
+        DataMappingPipeline pipeline = new DataMappingPipeline();
+        pipeline.setInput(new CsvPipelineInput().setFileSource(source).setFieldNamesInFirstRow(true));
+        pipeline.setSourceEntity(schema.getEntity("Raw"));
+        pipeline.setDataMapping(mapping);
+        pipeline.setTargetEntity(schema.getEntity("Processed"));
+        pipeline.setOutput(new ExcelPipelineOutput().setFileSink(sink).setFieldNamesInFirstRow(true));
+
+        //convert to Record
+        Record record = pipeline.toRecord();
+
+        //Create new DataMappingPipeline from the previous instance's configuration.
+        DataMappingPipeline pipelineFromRecord = new DataMappingPipeline().fromRecord(record);
+
+        System.out.println(pipelineFromRecord.toRecord());
+    }
+
+}
