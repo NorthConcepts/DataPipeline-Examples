8495a1ce0689ad4165b43dcb52850ebe132d7ee1
Joshi Gautam <gvjoshi25@gmail.com>
2021-06-28 16:52:07 +0530

    DP-1612 - Add Copyrights

--- src/main/java/com/northconcepts/datapipeline/foundations/examples/datamapping/DeclarativelyMapDataUsingPositions.java
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/datamapping/DeclarativelyMapDataUsingPositions.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2006-2021 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
 package com.northconcepts.datapipeline.foundations.examples.datamapping;
 
 import com.northconcepts.datapipeline.core.AsyncReader;
