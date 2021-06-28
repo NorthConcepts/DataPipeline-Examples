2f3456b3dcc34dbd305b2a1d7692fe8a8c32e52f
Gautam Joshi <gvjoshi25@gmail.com>
2021-06-28 17:37:05 +0530

    DP-1612 - Add Copyrights (#67)

--- src/main/java/com/northconcepts/datapipeline/examples/orc/WriteAnOrcFile.java
+++ src/main/java/com/northconcepts/datapipeline/examples/orc/WriteAnOrcFile.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2006-2021 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
 package com.northconcepts.datapipeline.examples.orc;
 
 import java.io.File;
