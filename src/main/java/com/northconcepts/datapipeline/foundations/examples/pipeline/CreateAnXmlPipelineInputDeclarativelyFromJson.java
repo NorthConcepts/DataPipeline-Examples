90c7b0d3d503c148e70524ab91621370f52d2d32
Bryan-Noel <noelbryansoftware@gmail.com>
2021-06-30 09:00:28 +0800

    DP-1621-Add_XmlPipelineInput_Java,_XML,_and_JSON_examples

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/pipeline/CreateAnXmlPipelineInputDeclarativelyFromJson.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2006-2021 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.foundations.examples.pipeline;
+
+
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.foundations.pipeline.Pipeline;
+import com.northconcepts.datapipeline.foundations.pipeline.input.XmlPipelineInput;
+import com.northconcepts.datapipeline.foundations.pipeline.output.DataWriterPipelineOutput;
+
+import java.io.FileInputStream;
+
+public class CreateAnXmlPipelineInputDeclarativelyFromJson {
+
+    public static void main(String[] args) throws Throwable {
+        Pipeline pipeline = new Pipeline();
+
+        XmlPipelineInput input = (XmlPipelineInput) new XmlPipelineInput()
+            .fromJson(new FileInputStream("example/data/input/pipeline/xmlpipelineinput.json"));
+
+        pipeline.setInput(input);
+        pipeline.setOutput(new DataWriterPipelineOutput(() -> StreamWriter.newSystemOutWriter()));
+
+        pipeline.run();
+    }
+
+}
