bca2f9dd9e0c35e3fda957f6189c7e64606e68fe
Bryan Noel <42918023+Bryan-Noel@users.noreply.github.com>
2021-07-04 06:08:42 +0800

    DP-1620-Add_JsonPipelineInput_Java,_XML,_and_JSON_examples (#69)
    * DP-1620-Add_JsonPipelineInput_Java,_XML,_and_JSON_examples
    
    * DP-1620-Add_JsonPipelineInput_Java,_XML,_and_JSON_examples

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/pipeline/CreateAJsonPipelineInput.java
@@ -0,0 +1,38 @@
+package com.northconcepts.datapipeline.foundations.examples.pipeline;
+
+import com.northconcepts.datapipeline.foundations.file.FileSource;
+import com.northconcepts.datapipeline.foundations.file.LocalFileSink;
+import com.northconcepts.datapipeline.foundations.file.LocalFileSource;
+import com.northconcepts.datapipeline.foundations.pipeline.Pipeline;
+import com.northconcepts.datapipeline.foundations.pipeline.input.JsonPipelineInput;
+import com.northconcepts.datapipeline.foundations.pipeline.output.ExcelPipelineOutput;
+import com.northconcepts.datapipeline.xml.XmlReader;
+
+public class CreateAJsonPipelineInput {
+
+    public static void main(String... args) {
+        Pipeline pipeline = new Pipeline();
+
+        FileSource source = new LocalFileSource()
+            .setName("input-json")
+            .setPath("example/data/input/pipeline/json-input.json")
+            .detectFileType();
+        
+        JsonPipelineInput input = new JsonPipelineInput()
+            .setFileSource(source)
+            .addRecordBreak("//array/object")
+            .addField("UID", "/object/UID", true)
+            .addField("type", "//favorites/*/node()")
+            .addField("favorite-name", "//favorites//name")
+            .addField("favorite-id", "//favorites//id", "//favorites/books")
+            .addField("favorite-category", "//favorites//category")
+            .setDuplicateFieldPolicy(XmlReader.DuplicateFieldPolicy.USE_LAST_VALUE);
+        
+        pipeline.setInput(input);
+
+        LocalFileSink sink = new LocalFileSink().setPath("data/output/test.xlsx");
+        pipeline.setOutput(new ExcelPipelineOutput().setFileSink(sink).setFieldNamesInFirstRow(true));
+
+        pipeline.run();
+    }
+}
