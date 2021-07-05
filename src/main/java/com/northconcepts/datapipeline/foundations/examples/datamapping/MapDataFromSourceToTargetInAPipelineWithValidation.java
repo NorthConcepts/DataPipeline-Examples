97ef88bd026521fe708c1552748cbe198ee3ff18
Bryan-Noel <noelbryansoftware@gmail.com>
2021-07-05 19:44:01 +0800

    DP-1697-Ensure_the_examples_updated_in_DP-1588_are_handle_by_other_examples

--- src/main/java/com/northconcepts/datapipeline/foundations/examples/datamapping/MapDataFromSourceToTargetInAPipelineWithValidation.java
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/datamapping/MapDataFromSourceToTargetInAPipelineWithValidation.java
@@ -6,23 +6,21 @@
  */
 package com.northconcepts.datapipeline.foundations.examples.datamapping;
 
-import java.io.File;
 import java.math.BigDecimal;
 
-import com.northconcepts.datapipeline.core.DataReader;
-import com.northconcepts.datapipeline.core.DataWriter;
 import com.northconcepts.datapipeline.core.FieldType;
 import com.northconcepts.datapipeline.core.StreamWriter;
-import com.northconcepts.datapipeline.csv.CSVReader;
 import com.northconcepts.datapipeline.filter.FieldCount;
 import com.northconcepts.datapipeline.foundations.datamapping.DataMapping;
-import com.northconcepts.datapipeline.foundations.datamapping.DataMappingReader;
 import com.northconcepts.datapipeline.foundations.datamapping.FieldMapping;
+import com.northconcepts.datapipeline.foundations.file.LocalFileSource;
+import com.northconcepts.datapipeline.foundations.pipeline.DataMappingPipeline;
+import com.northconcepts.datapipeline.foundations.pipeline.input.CsvPipelineInput;
+import com.northconcepts.datapipeline.foundations.pipeline.output.DataWriterPipelineOutput;
 import com.northconcepts.datapipeline.foundations.schema.EntityDef;
 import com.northconcepts.datapipeline.foundations.schema.NumericFieldDef;
 import com.northconcepts.datapipeline.foundations.schema.SchemaDef;
 import com.northconcepts.datapipeline.foundations.schema.TextFieldDef;
-import com.northconcepts.datapipeline.job.Job;
 
 public class MapDataFromSourceToTargetInAPipelineWithValidation {
 
@@ -37,21 +35,20 @@ public class MapDataFromSourceToTargetInAPipelineWithValidation {
         EntityDef productEntity = schema.getEntity("Product");
         
         DataMapping mapping = new DataMapping()
-                .setTargetValidationEntity(productEntity)   // set optional entity definition to validate against
                 .setValue("Markup", BigDecimal.valueOf(10.00))  // add a constant to be used in the mapping
                 .addFieldMapping(new FieldMapping("Title", "coalesce(source.Title, source.Handle)"))
                 .addFieldMapping(new FieldMapping("Cost", "${source.Variant Price}"))  // use ${} since field has space in name
                 .addFieldMapping(new FieldMapping("Price", "toBigDecimal(target.Cost) + Markup"));
-        
-        DataReader reader = new CSVReader(new File("data/input/jewelry.csv"))
-                .setAllowMultiLineText(true)
-                .setFieldNamesInFirstRow(true);
-        
-        reader = new DataMappingReader(reader, mapping);
-        
-        DataWriter writer = StreamWriter.newSystemOutWriter();
-        
-        Job.run(reader, writer);
+
+        LocalFileSource fileSource = new LocalFileSource().setPath("data/input/jewelry.csv");
+
+        DataMappingPipeline pipeline = new DataMappingPipeline()
+            .setDataMapping(mapping)
+            .setTargetEntity(productEntity)
+            .setInput(new CsvPipelineInput().setFileSource(fileSource).setAllowMultiLineText(true).setFieldNamesInFirstRow(true))
+            .setOutput(new DataWriterPipelineOutput(() -> StreamWriter.newSystemOutWriter()));
+
+        pipeline.run();
     }
 
 }
