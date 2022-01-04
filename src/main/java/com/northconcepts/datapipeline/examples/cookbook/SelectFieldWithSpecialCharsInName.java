17e0d4f2acd6e5084bfbaf925b568da4b6c981b5
Dele Taylor <dele.taylor@northconcepts.com>
2022-01-03 21:45:41 -0500

    DP-1975 add example to select fields with symbols in name

---
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/SelectFieldWithSpecialCharsInName.java
@@ -0,0 +1,30 @@
+package com.northconcepts.datapipeline.examples.cookbook;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.job.Job;
+import com.northconcepts.datapipeline.memory.MemoryReader;
+import com.northconcepts.datapipeline.transform.SelectFields;
+import com.northconcepts.datapipeline.transform.TransformingReader;
+
+public class SelectFieldWithSpecialCharsInName {
+
+    public static void main(String[] args) {
+        DataReader reader = new MemoryReader()
+                .add(new Record()
+                        .setField("ID", 123)
+                        .setField("Date .(dd-MMM-yyyy", "13-Jan-2021")
+                        .setField("Rate", 7.89)
+                        .setField("Hours", 10)
+                        );
+        reader = new TransformingReader(reader)
+                .add(new SelectFields("Hours", "Rate", "`Date .(dd-MMM-yyyy`"));
+
+        DataWriter writer = StreamWriter.newSystemOutWriter();
+        Job.run(reader, writer);
+
+    }
+
+}
