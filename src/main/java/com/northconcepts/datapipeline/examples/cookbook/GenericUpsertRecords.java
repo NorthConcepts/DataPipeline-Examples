c446a590e0d80637e7810bb66abefc89a5ce61c0
Dele Taylor <dele.taylor@northconcepts.com>
2022-02-27 08:11:08 -0500

    DP-2054 sync new examples with old URLs (#94)
    * DP-2054 Add missing upsert examples
    
    - update DP to 7.2
    
    * DP-2054 Ignore old license file

---
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/GenericUpsertRecords.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2006-2022 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.examples.cookbook;
+
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.northconcepts.datapipeline.core.DataException;
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.csv.CSVWriter;
+import com.northconcepts.datapipeline.jdbc.JdbcReader;
+import com.northconcepts.datapipeline.jdbc.JdbcUpsertWriter;
+import com.northconcepts.datapipeline.jdbc.upsert.GenericUpsert;
+import com.northconcepts.datapipeline.job.Job;
+
+public class GenericUpsertRecords {
+
+	public static void main(String[] args) {
+
+		final String DATABASE_DRIVER = "org.hsqldb.jdbcDriver";
+		final String DATABASE_URL = "jdbc:hsqldb:mem:aname";
+		final String DATABASE_USERNAME = "sa";
+		final String DATABASE_PASSWORD = "";
+		final String DATABASE_TABLE = "CreditBalance";
+		final String PRIMARY_KEY = "Account";
+
+		Connection connection;
+		try {
+			Class.forName(DATABASE_DRIVER);
+			connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+
+			createTable(connection);
+		} catch (Throwable e) {
+			throw DataException.wrap(e);
+		}
+
+		DataReader reader = new CSVReader(new File("example/data/input/credit-balance-02.csv")).setFieldNamesInFirstRow(true);
+		DataWriter writer = new JdbcUpsertWriter(connection, DATABASE_TABLE, new GenericUpsert().setInsertFirst(true), PRIMARY_KEY);
+
+		Job.run(reader, writer);
+
+		reader = new JdbcReader(connection, "Select * from CreditBalance;");
+		writer = new CSVWriter(new OutputStreamWriter(System.out));
+
+		Job.run(reader, writer);
+	}
+
+	public static void createTable(Connection connection) throws Throwable{
+		PreparedStatement preparedStatement;
+		String dropTableQuery = "DROP TABLE CreditBalance IF EXISTS;";
+		String createTableQuery = "CREATE TABLE CreditBalance ("
+			+ "Account INTEGER, "
+			+ "LastName VARCHAR(256), "
+			+ "FirstName VARCHAR(256), "
+			+ "Balance DOUBLE, "
+			+ "CreditLimit DOUBLE, "
+			+ "AccountCreated DATE, "
+			+ "Rating CHAR, "
+			+ "PRIMARY KEY (Account));";
+
+		preparedStatement = connection.prepareStatement(dropTableQuery);
+		preparedStatement.execute();
+
+		preparedStatement = connection.prepareStatement(createTableQuery);
+		preparedStatement.execute();
+	}
+}
