16565da4a9248e53774c03126319b6e6c3ae4d69
chlocarlos <chlophuricamulavu88@gmail.com>
2022-09-28 14:18:12 +0300

    DP-2544-change code to use Job.run(reader, new StreamWriter(System.out));

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadAllTabsFromAnExcelFile.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadAllTabsFromAnExcelFile.java
@@ -6,33 +6,23 @@
  */
 package com.northconcepts.datapipeline.examples.cookbook;
 
-import com.northconcepts.datapipeline.core.DataEndpoint;
 import com.northconcepts.datapipeline.core.DataReader;
-import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.core.StreamWriter;
 import com.northconcepts.datapipeline.excel.ExcelDocument;
 import com.northconcepts.datapipeline.excel.ExcelReader;
-import org.apache.log4j.Logger;
+import com.northconcepts.datapipeline.job.Job;
 
 import java.io.File;
 
 public class ReadAllTabsFromAnExcelFile {
-
-    public static final Logger log = DataEndpoint.log;
 
     public static void main(String[] args) throws Throwable {
         ExcelDocument document = new ExcelDocument().open(new File("example/data/input/data-with-multiple-tabs.xlsx"));
 
         for (String sheetName : document.getSheetNames()) {
             DataReader reader = new ExcelReader(document).setFieldNamesInFirstRow(true).setSheetName(sheetName);
-            reader.open();
-            try {
-                Record record;
-                while ((record = reader.read()) != null) {
-                    log.info(record);
-                }
-            } finally {
-                reader.close();
-            }
+
+            Job.run(reader, new StreamWriter(System.out));
         }
 
     }
