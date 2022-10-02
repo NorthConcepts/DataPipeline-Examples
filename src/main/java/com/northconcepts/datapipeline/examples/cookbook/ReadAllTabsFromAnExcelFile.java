52c0da6a936f60d65119f60af40a28fb3a6c4a3f
Chlophuric <chlophuricamulavu88@gmail.com>
2022-10-02 05:39:00 +0300

    DP-2544-Add example to read all tabs from Excel file (#112)
    * DP-2544-Add example to read all tabs from Excel file
    
    * DP-2544-change code to use Job.run(reader, new StreamWriter(System.out));
    
    * DP-2544-print sheetName
    
    * DP-2544-add different dataset to second tab and rename tabs
    
    * DP-2544-change mispelled address name

---
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadAllTabsFromAnExcelFile.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2006-2022 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.examples.cookbook;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.excel.ExcelDocument;
+import com.northconcepts.datapipeline.excel.ExcelReader;
+import com.northconcepts.datapipeline.job.Job;
+
+import java.io.File;
+
+public class ReadAllTabsFromAnExcelFile {
+
+    public static void main(String[] args) throws Throwable {
+        ExcelDocument document = new ExcelDocument().open(new File("example/data/input/data-with-multiple-tabs.xlsx"));
+
+        for (String sheetName : document.getSheetNames()) {
+            DataReader reader = new ExcelReader(document).setFieldNamesInFirstRow(true).setSheetName(sheetName);
+            System.out.println("SheetName:"+sheetName);
+            
+            Job.run(reader, new StreamWriter(System.out));
+        }
+
+    }
+}
