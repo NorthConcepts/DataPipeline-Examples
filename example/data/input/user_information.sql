7372b11d5741bbb2ab6c3ce53db861a7b087c207
Gautam Joshi <gvjoshi25@gmail.com>
2022-12-21 05:02:07 +0530

    DP-2798 - Write a Parquet file using schema from database (#117)
    * DP-2798 - Write a Parquet file using schema from database
    
    * Changes as per review comments
    
    * Updated table name in comments

---
+++ example/data/input/user_information.sql
@@ -0,0 +1,29 @@
+CREATE TABLE user_role (
+  user_role_id INT NOT NULL,
+  user_role VARCHAR(32) NOT NULL,
+  PRIMARY KEY (user_role_id)
+ );
+  
+CREATE TABLE user (
+  user_id INT NOT NULL,
+  user_name VARCHAR(16) NOT NULL,
+  first_name VARCHAR(255) NULL,
+  last_name VARCHAR(255) NULL,
+  email VARCHAR(255) NULL,
+  password VARCHAR(255) NOT NULL,
+  user_role_id INT NULL,
+  PRIMARY KEY (user_id),
+  FOREIGN KEY (user_role_id) REFERENCES user_role (user_role_id)
+  );
+
+
+INSERT INTO user_role
+(user_role_id, user_role)
+VALUES
+(1, 'Admin'), (2, 'Viewer');
+
+INSERT INTO user 
+(user_id, user_name, first_name, last_name, email, password, user_role_id) VALUES
+(1, 'john_doe', 'John', 'Doe', 'john.doe@example.com', '**********', 1),
+(2, 'jane_doe', 'jane', 'Doe', 'jane.doe@example.com', '**********', 1),
+(3, 'view_user', 'viewer', '', 'view_user@example.com', '**********', 2);
