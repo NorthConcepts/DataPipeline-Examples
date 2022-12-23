2b240dd0fe8581b12b85f627b5825bfcbbeff418
Gautam Joshi <gvjoshi25@gmail.com>
2022-12-23 15:56:03 +0530

    Updating data type in sql to illustrate examples accurately

--- example/data/input/jewellery.sql
+++ example/data/input/jewellery.sql
@@ -7,10 +7,11 @@ CREATE TABLE jewellery (
 	gift_card TINYINT,
 	variant_price FLOAT,
 	variant_compare_at_price DOUBLE,
-	min_price DECIMAL(10,0),
-	max_price DECIMAL(30,0),
-	auction_start_price DECIMAL(25,3),
-	sold_price DECIMAL(20,0),
+	min_price DECIMAL(5,0),
+	max_price DECIMAL(30,10),
+	height DECIMAL(15,0),
+	width DECIMAL(8,0),
+	weight DECIMAL(25,0),
 	taxable CHAR,
 	created_on TIMESTAMP,
 	launch_date DATE,
@@ -19,19 +20,23 @@ CREATE TABLE jewellery (
 
 INSERT INTO jewellery
 (title, published, variant_inventory_qty, gift_card,
-variant_price, variant_compare_at_price, min_price, max_price, auction_start_price, sold_price, taxable,
+variant_price, variant_compare_at_price, min_price, max_price,
+height, width, weight,
 created_on, launch_date, launch_time)
 VALUES
 ('7 Shakra Bracelet', false, 5, 0,
-123.123, 1234.1234, 100, 9765490, 1000.123, 67890567, 'N', 
+123.123, 1234.1234, 100, 9765490,
+12345, 98765, 12345678901234567890,
 '2022-09-25 21:35:13', '2022-09-25', '09:30:17'),
 
 ('Anchor Bracelet Mens', true, 10, 1,
-987.987, 5678.5678, 300, 1256793, 5600.987, 44889986, 'Y', 
+987.987, 5678.5678, 300, 1256793,
+67890, 456235, 98765432109876543210,
 '2022-12-21 21:35:13', '2023-01-12', '09:30:17'),
 
 ('Bangle Bracelet', true, 10, 0,
-123.123, 1234.1234, 100, 9765490, 1000.123, 67890567, 'Y', 
+123.123, 1234.1234, 100, 9765490,
+556677, 99441166, 11223344556677889900,
 '2022-09-25 21:35:13', '2022-09-25', '09:30:17')
 ;
 
