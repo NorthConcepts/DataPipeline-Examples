817c46800a8db4ff0db8371d6c5d9114e1419d48
Dele Taylor <dele.taylor@northconcepts.com>
2022-12-30 15:19:03 -0500

    DP-2817 reorder entities to make easier to understand

--- src/main/java/com/northconcepts/datapipeline/foundations/examples/schema/ValidateDataUsingNestedEntities.java
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/schema/ValidateDataUsingNestedEntities.java
@@ -13,36 +13,33 @@ import com.northconcepts.datapipeline.foundations.schema.ValidationResult;
 public class ValidateDataUsingNestedEntities {
 
     public static void main(String[] args) {
-        SchemaDef schema = new SchemaDef()
-                .addEntity(new EntityDef("suppliers")
-                        .setAllowExtraFieldsInValidation(false)
-                        .addField(new NumericFieldDef("supplier_id", FieldType.LONG).setPrimaryKeyPosition(0))
-                        .addField(new TextFieldDef("supplier_name", FieldType.STRING).setPrimaryKeyPosition(1))
-                        .addField(new TextFieldDef("country", FieldType.STRING))
-                        .addField(new RecordFieldDef("products_record_field", "products").setArray(true).setMaximumElements(5)));
+        SchemaDef schema = new SchemaDef();
 
-        schema
-        .addEntity(new EntityDef("products")
+        schema.addEntity(new EntityDef("products")
                 .addField(new NumericFieldDef("product_id", FieldType.LONG))
                 .addField(new TextFieldDef("product_name", FieldType.STRING)));
 
+        schema.addEntity(new EntityDef("suppliers")
+                .setAllowExtraFieldsInValidation(false)
+                .addField(new NumericFieldDef("supplier_id", FieldType.LONG).setPrimaryKeyPosition(0))
+                .addField(new TextFieldDef("supplier_name", FieldType.STRING).setPrimaryKeyPosition(1))
+                .addField(new TextFieldDef("country", FieldType.STRING))
+                .addField(new RecordFieldDef("products_record_field", "products").setArray(true).setMaximumElements(5)));
+
         Record magazineProduct = new Record()
-                .setField("product_id", 1) //This field should be LONG instead of INT
-                .setField("product_name", "Magazine")
-                ;
+                .setField("product_id", 1) // This field should be LONG instead of INT
+                .setField("product_name", "Magazine");
 
         Record newsPaperProduct = new Record()
-                .setField("product_id", 1) //This field should be LONG instead of INT
-                .setField("product_name", "News Paper")
-                ;
+                .setField("product_id", 1) // This field should be LONG instead of INT
+                .setField("product_name", "News Paper");
 
         Record suppierRecord = new Record()
-                .setField("supplier_id", 1) //This field should be LONG instead of INT
+                .setField("supplier_id", 1) // This field should be LONG instead of INT
                 .setField("supplier_name", "A1")
                 .setField("country", "USA")
                 .setField("city", "New York") // Unexpected field
-                .setField("products_record_field", new Record[] { magazineProduct, newsPaperProduct })
-                ;
+                .setField("products_record_field", new Record[] { magazineProduct, newsPaperProduct });
 
         System.out.println("==========================Input Record====================================");
         System.out.println(suppierRecord);
