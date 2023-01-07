c72ba6d9522cc4a23469855c0fabb4781cd71cfc
Gautam Joshi <gvjoshi25@gmail.com>
2023-01-08 00:52:07 +0530

    DP-2841 - Create An Example for FastRenameField (#133)
    * DP-2841 - Create An Example for FastRenameField
    
    * Removed comments

---
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/RenameAFieldQuicklyInFlatAndTabularData.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2006-2022 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.examples.cookbook;
+
+import java.io.File;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.job.Job;
+import com.northconcepts.datapipeline.transform.FastRenameField;
+import com.northconcepts.datapipeline.transform.TransformingReader;
+
+public class RenameAFieldQuicklyInFlatAndTabularData {
+
+    public static void main(String[] args) throws Throwable {
+        DataReader reader = new CSVReader(new File("example/data/input/credit-balance-01.csv"))
+                .setFieldNamesInFirstRow(true);
+
+        reader = new TransformingReader(reader)
+                .add(new FastRenameField("Rating", "Score")) // Rename field with name
+                .add(new FastRenameField(1, "last_name")) // Rename field "LastName" to "last_name" by index
+                .add(new FastRenameField(2, "first_name")) // Rename field "LastName" to "last_name" by index
+                ;
+
+        Job.run(reader, new StreamWriter(System.out));
+    }
+
+}
