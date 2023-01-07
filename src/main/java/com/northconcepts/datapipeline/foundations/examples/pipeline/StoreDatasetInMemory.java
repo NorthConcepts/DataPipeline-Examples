d37015e0be4dc81ac3e67bf573e3260c27a7bb38
chlocarlos <chlophuricamulavu88@gmail.com>
2023-01-07 10:34:26 +0300

    DP-2815 show data from dataset

--- src/main/java/com/northconcepts/datapipeline/foundations/examples/pipeline/StoreDatasetInMemory.java
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/pipeline/StoreDatasetInMemory.java
@@ -1,5 +1,7 @@
 package com.northconcepts.datapipeline.foundations.examples.pipeline;
 
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.Record;
 import com.northconcepts.datapipeline.csv.CSVReader;
 import com.northconcepts.datapipeline.foundations.pipeline.Pipeline;
 import com.northconcepts.datapipeline.foundations.pipeline.dataset.Dataset;
@@ -10,15 +12,19 @@ import java.io.File;
 public class StoreDatasetInMemory {
 
     public static void main(String[] args) {
-        Pipeline pipeline = new Pipeline()
-                .setInputAsDataReaderFactory(() -> new CSVReader(new File("example/data/input/credit-balance-01.csv"))
-                        .setFieldNamesInFirstRow(true));
-        
+        DataReader reader = new CSVReader(new File("example/data/input/credit-balance-01.csv"))
+                .setFieldNamesInFirstRow(true);
+
+        Pipeline pipeline = new Pipeline().setInputAsDataReader(reader);
+
         Dataset dataset = new MemoryDataset(pipeline);
-        
+
         dataset.load().waitForRecordsToLoad();
 
+        for (Record record : dataset) {
+            System.out.println(record);
+        }
         dataset.close();
     }
-    
+
 }
