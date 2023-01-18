a48851342f854a08c8bae1244ab7af2a2c9dde63
Gautam Joshi <gvjoshi25@gmail.com>
2023-01-18 15:26:01 +0530

    DP-2893 - Serialize DataMapping containing special characters to XML

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/datamapping/SerializeDataMappingContainingSpecialCharactersToXML.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2006-2023 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.foundations.examples.datamapping;
+
+import com.northconcepts.datapipeline.foundations.datamapping.DataMapping;
+
+public class SerializeDataMappingContainingSpecialCharactersToXML {
+
+    public static void main(String[] args) {
+        DataMapping mapping = new DataMapping()
+                // Description contains special character SOH (start of heading)
+                .setDescription("DataMapping for user table having start of heading character" + ((char) 01))
+                .addFieldMapping("first_name", "source.fname")
+                .addFieldMapping("last_name", "toUpperCase(source.lname)")
+                .addFieldMapping("name", "source.fname + ' ' + target.last_name")
+                .addFieldMapping("name_length", "length(target.name)");
+
+        /*
+         * In DP, default XML default version 1.0 is used in XML Serialization.
+         * From JDK 19 onwards, to serialize such special characters, XML version 1.1 is required.
+         * To Enable XML version 1.1 declaration, consider this snippet.
+         */
+        System.out.println(mapping.toXml(true));
+    }
+
+}
