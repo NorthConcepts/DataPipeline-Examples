14bf9f4b674a850002c87f6af14040be0fc878c1
Dele Taylor <dele.taylor@northconcepts.com>
2023-01-22 14:54:01 -0500

    DP-2808 make example easier to understand

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadAndTransformDataInParallel.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadAndTransformDataInParallel.java
@@ -1,31 +1,37 @@
 package com.northconcepts.datapipeline.examples.cookbook;
 
+import java.io.File;
+
 import com.northconcepts.datapipeline.core.AsyncTaskReader;
 import com.northconcepts.datapipeline.core.DataReader;
 import com.northconcepts.datapipeline.core.SortingReader;
 import com.northconcepts.datapipeline.core.StreamWriter;
 import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.job.DataReaderDecorator;
 import com.northconcepts.datapipeline.job.Job;
 import com.northconcepts.datapipeline.transform.BasicFieldTransformer;
 import com.northconcepts.datapipeline.transform.RenameField;
 import com.northconcepts.datapipeline.transform.TransformingReader;
 
-import java.io.File;
-
 public class ReadAndTransformDataInParallel {
+
+    private static final int THREADS = 4;
 
     public static void main(String[] args) {
 
-        DataReader input = new CSVReader(new File("example/data/input/credit-balance-02-1000000.csv"))
-            .setFieldNamesInFirstRow(true);
+        DataReader reader = new CSVReader(new File("example/data/input/credit-balance-02-1000000.csv"))
+                .setFieldNamesInFirstRow(true);
 
-        DataReader reader = new AsyncTaskReader(input, (originalDataReader) ->
-            new TransformingReader(originalDataReader)
-                .add(new RenameField("Rating", "AccountRating"))
-                .add(new BasicFieldTransformer("Balance").stringToDouble().nullToValue(0.0))
-            , 4);
+        reader = new AsyncTaskReader(reader, createTaskToRunInMultipleThreads(), THREADS);
 
         Job.run(new SortingReader(reader).add("Balance"), StreamWriter.newSystemOutWriter());
     }
+
+    private static DataReaderDecorator createTaskToRunInMultipleThreads() {
+        return (originalDataReader) -> new TransformingReader(originalDataReader)
+                .add(new RenameField("Rating", "AccountRating"))
+                .add(new BasicFieldTransformer("Balance").stringToDouble().nullToValue(0.0));
+    }
+
 }
 
