797582eaf516a2ccb548a96e724cebe8b1fde5e2
Gautam Joshi <gvjoshi25@gmail.com>
2023-01-29 21:31:37 +0530

    DP-2922 - Add Primary Keys to Entities

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/schema/AddPrimaryKeysToEntities.java
@@ -0,0 +1,37 @@
+package com.northconcepts.datapipeline.foundations.examples.schema;
+
+import com.northconcepts.datapipeline.core.FieldType;
+import com.northconcepts.datapipeline.foundations.schema.EntityDef;
+import com.northconcepts.datapipeline.foundations.schema.NumericFieldDef;
+import com.northconcepts.datapipeline.foundations.schema.SchemaDef;
+import com.northconcepts.datapipeline.foundations.schema.TextFieldDef;
+
+public class AddPrimaryKeysToEntities {
+
+    public static void main(String[] args) {
+        SchemaDef schema = new SchemaDef("E-Commerce Schema");
+        
+        schema.addEntity(new EntityDef("suppliers")
+                // Primary Key
+                .addField(new NumericFieldDef("supplier_id", FieldType.LONG).setPrimaryKeyPosition(0))
+                .addField(new TextFieldDef("supplier_name", FieldType.STRING))
+                .addField(new TextFieldDef("city", FieldType.STRING))
+                .addField(new TextFieldDef("country", FieldType.STRING))
+                .addField(new TextFieldDef("contact_details", FieldType.STRING))
+                );
+
+        schema.addEntity(new EntityDef("products")
+                // Composite key of product_id & supplier_id
+                .addField(new NumericFieldDef("product_id", FieldType.LONG).setPrimaryKeyPosition(0))
+                .addField(new NumericFieldDef("supplier_id", FieldType.LONG).setPrimaryKeyPosition(1))
+                .addField(new TextFieldDef("product_name", FieldType.STRING))
+                .addField(new TextFieldDef("color", FieldType.STRING))
+                );
+
+        System.out.println("=========================================================");
+        System.out.println(schema.getEntity("suppliers").getPrimaryKeyFields());
+        System.out.println("=========================================================");
+        System.out.println(schema.getEntity("products").getPrimaryKeyFields());
+        System.out.println("=========================================================");
+    }
+}
