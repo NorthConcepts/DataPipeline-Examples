e080c21d55a89c5c8ebf248bb826be71216f2a4d
chlocarlos <chlophuricamulavu88@gmail.com>
2023-01-30 17:40:39 +0300

    DP-2917-Add MailChimp Example

---
+++ src/main/java/com/northconcepts/datapipeline/mailchimp/ReadMailchimpContacts.java
@@ -0,0 +1,25 @@
+package com.northconcepts.datapipeline.mailchimp;
+
+import com.northconcepts.datapipeline.mailchimp.list.ListMember;
+import com.northconcepts.datapipeline.mailchimp.list.ListMemberStatus;
+import com.northconcepts.datapipeline.mailchimp.list.ListMembers;
+
+public class ReadMailchimpContacts {
+    private static final String apiKey = "api-key";
+    private static final String listId = "list-id";
+
+    private static final MailChimpClient client = MailChimpClient.Proxy.get(apiKey);
+    
+    public static void main(String[] args) {
+        ListMembers listMembers = client.getListMembersObject(listId, ListMemberStatus.subscribed, 0, 5);
+        
+        System.out.println("Total contacts :"+listMembers.getMembers().size());
+        for (ListMember listMember: listMembers.getMembers()) {
+            System.out.println("---------------------------");
+            System.out.println("Id :"+listMember.getId());
+            System.out.println("Email :"+listMember.getEmailAddress());
+            System.out.println("Status :"+listMember.getStatus());
+        }
+    }
+    
+}
