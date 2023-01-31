589c26e9bb25707a6a698ffedb56376792f100e8
Chlophuric <chlophuricamulavu88@gmail.com>
2023-01-31 14:44:55 +0300

    DP-2917-Add MailChimp Example (#150)
    * DP-2917-Add MailChimp Example
    
    * DP-2917-Add MailChimp Example using MailChimpListMemberReader
    
    * DP-2917-remove limit reader

---
+++ src/main/java/com/northconcepts/datapipeline/mailchimp/ReadMailChimpContacts.java
@@ -0,0 +1,23 @@
+package com.northconcepts.datapipeline.mailchimp;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.job.Job;
+import com.northconcepts.datapipeline.mailchimp.list.ListMemberStatus;
+
+public class ReadMailChimpContacts {
+
+    private static final String apiKey = "api-key";
+    private static final String listId = "list-id";
+    
+    public static void main(String[] args) {
+        DataReader reader = new MailChimpListMemberReader(apiKey, listId)
+                .setStatus(ListMemberStatus.subscribed);
+        
+        DataWriter writer = StreamWriter.newSystemOutWriter();
+        
+        Job.run(reader, writer);
+    }
+    
+}
