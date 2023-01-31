206e181b617fae362f50263965efc5cdfc4b39e2
Gautam Joshi <gvjoshi25@gmail.com>
2023-01-31 17:35:48 +0530

    DP-2927 - JIRA Related Examples (#151)
    * DP-2927 - JIRA Related Examples
    
    * Adding copyright
    
    * DP-2930 - Search for Jira Issues by Multiple IDs
    
    * Uploading correct file for SearchJiraIssuesByMultipleIds

---
+++ src/main/java/com/northconcepts/datapipeline/examples/jira/AddJiraComments.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2006-2023 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.examples.jira;
+
+import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.jira.client.JiraService;
+
+public class AddJiraComments {
+
+    private static final String JIRA_DOMAIN = "JIRA_DOMAIN";
+    private static final String JIRA_USERNAME = "USERNAME";
+    private static final String JIRA_API_KEY = "API_KEY";
+
+    private static final String JIRA_ISSUE_ID = "ISSUE_ID";
+    
+    public static void main(String... args) {
+        JiraService service = new JiraService(JIRA_DOMAIN, JIRA_USERNAME, JIRA_API_KEY);
+        
+        Record record = service.addIssueComment(JIRA_ISSUE_ID, "What is the progress on this issue?");
+        System.out.println(record);
+    }
+}
