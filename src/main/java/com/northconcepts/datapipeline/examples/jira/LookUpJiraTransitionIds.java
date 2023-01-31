be06c4798b94c86dd802b161749a33a63f557b60
Gautam Joshi <gvjoshi25@gmail.com>
2023-01-31 17:54:32 +0530

    DP-2927 - Look up Jira Transition Ids (#152)

---
+++ src/main/java/com/northconcepts/datapipeline/examples/jira/LookUpJiraTransitionIds.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2006-2023 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.examples.jira;
+
+import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.jira.client.JiraService;
+
+public class LookUpJiraTransitionIds {
+
+    private static final String JIRA_DOMAIN = "JIRA_DOMAIN";
+    private static final String JIRA_USERNAME = "USERNAME";
+    private static final String JIRA_API_KEY = "API_KEY";
+
+    public static void main(String[] args) {
+        JiraService service = new JiraService(JIRA_DOMAIN, JIRA_USERNAME, JIRA_API_KEY);
+
+        Record record = service.getIssueTransitions("AB-12");
+        System.out.println(record);
+    }
+}
