28f010efcb507c15d191c0ef8618c06d8bfe3e14
chlocarlos <chlophuricamulavu88@gmail.com>
2023-01-31 12:52:33 +0300

    DP-2917-remove limit reader

--- src/main/java/com/northconcepts/datapipeline/mailchimp/ReadMailChimpContacts.java
+++ src/main/java/com/northconcepts/datapipeline/mailchimp/ReadMailChimpContacts.java
@@ -2,7 +2,6 @@ package com.northconcepts.datapipeline.mailchimp;
 
 import com.northconcepts.datapipeline.core.DataReader;
 import com.northconcepts.datapipeline.core.DataWriter;
-import com.northconcepts.datapipeline.core.LimitReader;
 import com.northconcepts.datapipeline.core.StreamWriter;
 import com.northconcepts.datapipeline.job.Job;
 import com.northconcepts.datapipeline.mailchimp.list.ListMemberStatus;
@@ -15,8 +14,6 @@ public class ReadMailChimpContacts {
     public static void main(String[] args) {
         DataReader reader = new MailChimpListMemberReader(apiKey, listId)
                 .setStatus(ListMemberStatus.subscribed);
-        
-        reader = new LimitReader(reader, 5);
         
         DataWriter writer = StreamWriter.newSystemOutWriter();
         
