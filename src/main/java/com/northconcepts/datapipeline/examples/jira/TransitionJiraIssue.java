d117445287bd8f782786d924d2c0b1746365d535
Gautam Joshi <gvjoshi25@gmail.com>
2023-01-31 16:13:42 +0530

    Adding copyright

--- src/main/java/com/northconcepts/datapipeline/examples/jira/TransitionJiraIssue.java
+++ src/main/java/com/northconcepts/datapipeline/examples/jira/TransitionJiraIssue.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2006-2023 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
 package com.northconcepts.datapipeline.examples.jira;
 
 import com.northconcepts.datapipeline.jira.client.JiraService;
