eb3b1e72bddded10d6e280b7898cdda780b07b52
Gautam Joshi <gvjoshi25@gmail.com>
2023-02-18 19:30:41 +0530

    Changes as per review comments

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/datamapping/MapDataUsingAutoMappingAndExcludedFields.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2006-2023 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.foundations.examples.datamapping;
+
+import java.io.File;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.foundations.datamapping.DataMapping;
+import com.northconcepts.datapipeline.foundations.datamapping.DataMappingReader;
+import com.northconcepts.datapipeline.job.Job;
+
+public class MapDataUsingAutoMappingAndExcludedFields {
+
+    public static void main(String[] args) {
+        DataMapping mapping = new DataMapping()
+                .setAutoMapping(true) // enable auto fields mapping
+                .addExcludedFields("AccountCreated", "Rating") // exclude fields from auto mapping.
+                .addFieldMapping("Full_Name", "source.LastName+ ' ' + source.FirstName"); // custom field mapping. (optional)
+        
+        DataReader reader = new CSVReader(new File("example/data/input/bank_account.csv"))
+                .setAllowMultiLineText(true)
+                .setFieldNamesInFirstRow(true);
+
+        reader = new DataMappingReader(reader, mapping);
+
+        Job.run(reader, new StreamWriter(System.out));
+    }
+
+}
