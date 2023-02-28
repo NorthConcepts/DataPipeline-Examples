4b4682aee391606f66da0a719478c81329c3f827
Chlophuric <chlophuricamulavu88@gmail.com>
2023-02-28 05:13:15 +0300

    DP-2995 - Add example to load mapping from XML and automatically map da… (#156)
    * DP-2995 add example to load mapping from XML and automatically map data and exclude fields
    
    * DP-2995 change method name to AutomaticallyMapDataAndExcludeFieldsDeclaratively
    
    * DP-2995 change method name to AutomaticallyMapDataAndExcludeFieldsDeclaratively

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/datamapping/AutomaticallyMapDataAndExcludeFieldsDeclaratively.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2006-2023 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ * 
+ * https://northconcepts.com/data-pipeline/licensing/
+ */
+package com.northconcepts.datapipeline.foundations.examples.datamapping;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.foundations.datamapping.DataMapping;
+import com.northconcepts.datapipeline.foundations.datamapping.DataMappingReader;
+import com.northconcepts.datapipeline.job.Job;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class AutomaticallyMapDataAndExcludeFieldsDeclaratively {
+
+    public static void main(String[] args) throws Throwable {
+    
+        //Load DataMapping from this XML File.
+        DataMapping mappingFromXml = new DataMapping()
+                .fromXml(new FileInputStream("example/data/input/datamapping/customer-details-data-mapping.xml"));
+
+        DataReader reader = new CSVReader(new File("example/data/input/bank_account.csv"))
+                .setAllowMultiLineText(true)
+                .setFieldNamesInFirstRow(true);
+
+        reader = new DataMappingReader(reader, mappingFromXml);
+
+        Job.run(reader, new StreamWriter(System.out));
+    }
+
+}
