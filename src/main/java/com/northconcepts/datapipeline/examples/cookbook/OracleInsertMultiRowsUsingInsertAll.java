e8c3d0e20223d5f6e01a965dabc8c15b5ebacf45
yaphet17 <yafetberhanu3@gmail.com>
2023-04-02 08:25:11 +0300

    DP-2970_Improve example for ArrayValues and replace lambda with method reference

---
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/OracleInsertMultiRowsUsingInsertAll.java
@@ -0,0 +1,66 @@
+package com.northconcepts.datapipeline.examples.cookbook;
+
+import com.northconcepts.datapipeline.core.DataException;
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.csv.CSVWriter;
+import com.northconcepts.datapipeline.jdbc.JdbcReader;
+import com.northconcepts.datapipeline.jdbc.JdbcWriter;
+import com.northconcepts.datapipeline.jdbc.insert.OracleMultiRowInsertAllStatementInsert;
+import com.northconcepts.datapipeline.job.Job;
+
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class OracleInsertMultiRowsUsingInsertAll {
+
+    public static void main(String[] args) {
+
+        final String DATABASE_DRIVER = "org.hsqldb.jdbcDriver";
+        final String DATABASE_URL = "jdbc:hsqldb:mem:aname";
+        final String DATABASE_USERNAME = "sa";
+        final String DATABASE_PASSWORD = "";
+        final String DATABASE_TABLE = "CreditBalance";
+
+        Connection connection;
+        try {
+            Class.forName(DATABASE_DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+
+            createTable(connection);
+        } catch (Throwable e) {
+            throw DataException.wrap(e);
+        }
+
+        DataReader reader = new CSVReader(new File("example/data/input/credit-balance-insert-records.csv"))
+                .setFieldNamesInFirstRow(true);
+        DataWriter writer = new JdbcWriter(connection, DATABASE_TABLE, new OracleMultiRowInsertAllStatementInsert().setDebug(true))
+                .setBatchSize(3); // set batch size
+
+        Job.run(reader, writer);
+
+        reader = new JdbcReader(connection, "Select * from CreditBalance;");
+        writer = new CSVWriter(new OutputStreamWriter(System.out));
+
+        Job.run(reader, writer);
+    }
+
+    public static void createTable(Connection connection) throws Throwable{
+        PreparedStatement preparedStatement;
+        String createTableQuery = "CREATE TABLE CreditBalance ("
+                + "Account INTEGER, "
+                + "LastName VARCHAR(256), "
+                + "FirstName VARCHAR(256), "
+                + "Balance DOUBLE, "
+                + "CreditLimit DOUBLE, "
+                + "Rating CHAR, "
+                + "PRIMARY KEY (Account));";
+
+        preparedStatement = connection.prepareStatement(createTableQuery);
+        preparedStatement.execute();
+    }
+}
