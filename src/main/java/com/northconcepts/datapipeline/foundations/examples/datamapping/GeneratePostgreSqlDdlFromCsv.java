d4a086bf9c111f883151ef7bae5a6e0c0713ef55
Chlophuric <chlophuricamulavu88@gmail.com>
2023-05-04 00:19:43 +0300

    Dp 3120 - Generate postgre sql ddl from csv (#164)
    * DP-3119 - CreatePostgreSqlDdl from Excel
    
    * DP-3110 - Add postgresql dependency
    
    * DP-3120 - generate postgress ddl from csv
    
    * DP-3110 - use modern xlsx file
    
    * DP-3110 - remove unused import
    
    * DP-3120 - change schema name

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/datamapping/GeneratePostgreSqlDdlFromCsv.java
@@ -0,0 +1,31 @@
+package com.northconcepts.datapipeline.foundations.examples.datamapping;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.foundations.schema.EntityDef;
+import com.northconcepts.datapipeline.foundations.schema.SchemaDef;
+import com.northconcepts.datapipeline.foundations.tools.GenerateEntityFromDataset;
+import com.northconcepts.datapipeline.sql.postgresql.CreatePostgreSqlDdlFromSchemaDef;
+
+import java.io.File;
+
+public class GeneratePostgreSqlDdlFromCsv {
+
+    public static void main(String[] args) {
+        DataReader reader = new CSVReader(new File("example/data/input/credit-balance-01.csv"))
+                .setFieldNamesInFirstRow(true);
+
+        GenerateEntityFromDataset generator = new GenerateEntityFromDataset();
+        EntityDef entity = generator.generateEntity(reader).setName("creditBalance");
+
+        SchemaDef schema = new SchemaDef("accountInfo")
+                .addEntity(entity);
+
+        CreatePostgreSqlDdlFromSchemaDef postgresDdl = new CreatePostgreSqlDdlFromSchemaDef(schema)
+                .setPretty(true)
+                .setCheckIfDropTableExists(false);
+
+        System.out.println(postgresDdl);
+    }
+    
+}
