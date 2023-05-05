e37ad94e39891d4a26a95b65f52904f071f7dbf2
Chlophuric <chlophuricamulavu88@gmail.com>
2023-05-05 22:35:32 +0300

    Dp 3130 - Generate parquet schema by analyzing the first 500 records (#165)
    * DP-3130 Generate Parquet Schema By Analyzing Specified Number Of Records
    
    * DP-3130 -create parquet schema by analyzin all records
    
    * DP-3130 remove spaces in field names since Parquet doesn't allow it; rename example class
    
    * DP-3130 remove spaces in field names since Parquet doesn't allow it
    
    ---------
    
    Co-authored-by: Dele Taylor <dele.taylor@northconcepts.com>

---
+++ src/main/java/com/northconcepts/datapipeline/examples/parquet/GenerateParquetSchemaByAnalyzingAllRecords.java
@@ -0,0 +1,57 @@
+package com.northconcepts.datapipeline.examples.parquet;
+
+import java.io.File;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.Field;
+import com.northconcepts.datapipeline.core.ProxyReader;
+import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.job.Job;
+import com.northconcepts.datapipeline.parquet.ParquetDataReader;
+import com.northconcepts.datapipeline.parquet.ParquetDataWriter;
+
+public class GenerateParquetSchemaByAnalyzingAllRecords {
+
+    private static final Long RECORDS_TO_ANALYZE = null;
+    
+    private static final File PARQUET_FILE = new File("example/data/output/GenerateParquetSchemaForAllRecords.parquet");
+
+    public static void main(String[] args) {
+        DataReader reader = new CSVReader(new File("example/data/input/1000_Sales_Records.csv"))
+                .setFieldNamesInFirstRow(true);
+        
+        reader = fixFieldNames(reader);
+
+        ParquetDataWriter writer = new ParquetDataWriter(PARQUET_FILE)
+                .setMaxRecordsAnalyzed(RECORDS_TO_ANALYZE);
+        
+        Job.run(reader, writer);
+
+        System.out.println("============================================================");
+        System.out.println("Prepared Schema");
+        System.out.println("============================================================");
+
+        System.out.println(writer.getSchema());
+
+        System.out.println("============================================================");
+        System.out.println("Read the parquet file");
+        System.out.println("============================================================");
+
+        Job.run(new ParquetDataReader(PARQUET_FILE), new StreamWriter(System.out));
+    }
+    
+    private static ProxyReader fixFieldNames(DataReader reader) {
+        return new ProxyReader(reader){
+            @Override
+            protected Record interceptRecord(Record record) throws Throwable {
+                for (Field field : record) {
+                    field.setName(field.getName().replace(" ", "_"));
+                }
+                return record;
+            }
+        };
+    }
+    
+}
---
+++ src/main/java/com/northconcepts/datapipeline/examples/parquet/GenerateParquetSchemaByAnalyzingInitialRecords.java
@@ -0,0 +1,56 @@
+package com.northconcepts.datapipeline.examples.parquet;
+
+import java.io.File;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.Field;
+import com.northconcepts.datapipeline.core.ProxyReader;
+import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.job.Job;
+import com.northconcepts.datapipeline.parquet.ParquetDataReader;
+import com.northconcepts.datapipeline.parquet.ParquetDataWriter;
+
+public class GenerateParquetSchemaByAnalyzingInitialRecords {
+
+    private static final Long INITIAL_RECORDS_TO_ANALYZE = 500L;
+    
+    private static final File PARQUET_FILE = new File("example/data/output/GenerateParquetSchemaFor500Records.parquet");
+
+    public static void main(String[] args) {
+        DataReader reader = new CSVReader(new File("example/data/input/1000_Sales_Records.csv"))
+                .setFieldNamesInFirstRow(true);
+        
+        reader = fixFieldNames(reader);
+
+        ParquetDataWriter writer = new ParquetDataWriter(PARQUET_FILE);
+        writer.setMaxRecordsAnalyzed(INITIAL_RECORDS_TO_ANALYZE);
+        Job.run(reader, writer);
+
+        System.out.println("============================================================");
+        System.out.println("Prepared Schema");
+        System.out.println("============================================================");
+
+        System.out.println(writer.getSchema());
+
+        System.out.println("============================================================");
+        System.out.println("Read the parquet file");
+        System.out.println("============================================================");
+
+        Job.run(new ParquetDataReader(PARQUET_FILE), new StreamWriter(System.out));
+    }
+    
+    private static ProxyReader fixFieldNames(DataReader reader) {
+        return new ProxyReader(reader){
+            @Override
+            protected Record interceptRecord(Record record) throws Throwable {
+                for (Field field : record) {
+                    field.setName(field.getName().replace(" ", "_"));
+                }
+                return record;
+            }
+        };
+    }
+    
+}
