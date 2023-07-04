8aac9ef62e6ecf264dac5f5a93d785b1d795c8d6
Gautam Joshi <gvjoshi25@gmail.com>
2023-07-04 18:12:53 +0530

    Updated expression details

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/ParseAnExpression.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/ParseAnExpression.java
@@ -7,12 +7,12 @@ import com.northconcepts.datapipeline.internal.parser.Parser;
 public class ParseAnExpression {
 
     public static void main(String[] args) {
-        Expression expression = Parser.parseExpression("2 + 2");
+        Expression expression = Parser.parseExpression("(a + b) * 2");
                 
         DefaultExpressionContext expressionContext = new DefaultExpressionContext();
-        Long evaluatedValue = (Long) expression.evaluate(expressionContext);
-        System.out.println("2 + 2 = " + evaluatedValue);
+        expressionContext.setValue("a", 2);
+        expressionContext.setValue("b", 2);
         
-        // Learn more about expression language at: https://northconcepts.com/docs/expression-language
+        System.out.println("(a + b) * 2 = " + expression.evaluate(expressionContext));
     }
 }
