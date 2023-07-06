7da02edea00856f23dc69633f6104014856cf19e
Gautam Joshi <gvjoshi25@gmail.com>
2023-07-07 02:56:08 +0530

    DP-3223 - Remove copyright from examples (#169)
    * DP-3223 - Remove copyright from examples repository
    
    * Deleted local example file
    
    * Adding license file

--- src/main/java/com/northconcepts/datapipeline/foundations/examples/decisiontree/EvaluateADecisionTreeWithLookup.java
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/decisiontree/EvaluateADecisionTreeWithLookup.java
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2006-2022 North Concepts Inc.  All rights reserved.
- * Proprietary and Confidential.  Use is subject to license terms.
- * 
- * https://northconcepts.com/data-pipeline/licensing/
- */
 package com.northconcepts.datapipeline.foundations.examples.decisiontree;
 
 import com.northconcepts.datapipeline.core.FieldList;
