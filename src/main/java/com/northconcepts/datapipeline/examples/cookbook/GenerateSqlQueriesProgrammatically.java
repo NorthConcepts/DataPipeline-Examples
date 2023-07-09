cee868f4e1e678a29ceb5bc447af4c47cdb35e41
Gautam Joshi <gvjoshi25@gmail.com>
2023-07-09 16:06:38 +0530

    Updated example with SQL Select query parameters

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/GenerateSqlQueriesProgrammatically.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/GenerateSqlQueriesProgrammatically.java
@@ -7,11 +7,15 @@ public class GenerateSqlQueriesProgrammatically {
     public static void main(String[] args) {
         Select select = new Select("user")
                 .select("*")
-                .where("id = ?", 12)
+                .where("id = ? AND role = ?", 12, "admin")
                 .orderBy("firstName");
         
         String query = select.getSqlFragment();
         System.out.println("Select query: " + query);
         
+        System.out.println("Query parameters: ");
+        for (Object value : select.getParameterValues()) {
+            System.out.println(value.toString());
+        }
     }
 }
