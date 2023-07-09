d63f9993fe37ea4572a6689501b958b6e3aeb2ce
Gautam Joshi <gvjoshi25@gmail.com>
2023-07-09 15:55:50 +0530

    changes as per review comments

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/ParseInvalidExpressions.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/ParseInvalidExpressions.java
@@ -16,7 +16,10 @@ public class ParseInvalidExpressions {
         // Parse an invalid expression which is missing closing parenthesis
 
         Expression expression = Parser.parseExpression("toInt('123'", true);
+        executeExpression(expression);
+    }
 
+    private static void executeExpression(Expression expression) {
         InvalidSyntax invalidSyntax = (InvalidSyntax) expression;
         System.out.println("Source Expression: " + invalidSyntax.getSourceString());
         System.out.println("Expression Parse Exception: " + invalidSyntax.getParseException().getMessage());
@@ -33,28 +36,12 @@ public class ParseInvalidExpressions {
 
         Parser.doWithoutParseException(() -> {
             Expression expression1 = Parser.parseExpression("toInt('123'");
-            InvalidSyntax invalidSyntax1 = (InvalidSyntax) expression1;
-            System.out.println("Source Expression - 1: " + invalidSyntax1.getSourceString());
-            System.out.println("Expression Parse Exception - 1: " + invalidSyntax1.getParseException().getMessage());
-            
-            try {
-                System.out.println(expression1.evaluate(null));
-            } catch (Throwable e) {
-                System.err.println("Exception while evaluating invalid expression: " + e.getMessage());
-            }
+            executeExpression(expression1);
             
             System.out.println("=============================================================================");
             
             Expression expression2 = Parser.parseExpression("toInt('345'");
-            InvalidSyntax invalidSyntax2 = (InvalidSyntax) expression2;
-            System.out.println("Source Expression - 2: " + invalidSyntax2.getSourceString());
-            System.out.println("Expression Parse Exception - 2: " + invalidSyntax2.getParseException().getMessage());
-            
-            try {
-                System.out.println(expression2.evaluate(null));
-            } catch (Throwable e) {
-                System.err.println("Exception while evaluating invalid expression: " + e.getMessage());
-            }
+            executeExpression(expression2);
         });
     }
 }
