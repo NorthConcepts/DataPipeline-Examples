61d185994df3c32b2a780be5bfde78dc5e152ee6
sah2020 <akmaljonsamandarov@gmail.com>
2023-08-18 17:51:01 +0500

    DP-3447 Change db connection to hsqldb

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadACsvFileUsingMultithreading.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadACsvFileUsingMultithreading.java
@@ -4,34 +4,33 @@ import com.northconcepts.datapipeline.core.AsyncReader;
 import com.northconcepts.datapipeline.core.DataReader;
 import com.northconcepts.datapipeline.core.DataWriter;
 import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.examples.database.DB;
 import com.northconcepts.datapipeline.jdbc.JdbcWriter;
 import com.northconcepts.datapipeline.job.Job;
 
 import java.io.File;
 import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class ReadACsvFileUsingMultithreading {
     public static void main(String[] args) throws Throwable {
         DataReader reader = new CSVReader(new File("example/data/input/credit-balance-01.csv"))
                 .setFieldNamesInFirstRow(true);
-
-        DataWriter writer = new JdbcWriter(getJdbcConnection(), "dp_credit_balance")
-                .setAutoCloseConnection(true)
-                .setBatchSize(100);
-
         reader = new AsyncReader(reader).setMaxBufferSizeInBytes(1024 * 1024 * 10);
 
-        Job.run(reader, writer);
-    }
+        DB db = new DB();
+        Connection connection = db.getConnection();
+        db.execute("CREATE TABLE credit_balance (" +
+                " account INT NOT NULL," +
+                " lastname VARCHAR(32)," +
+                " firstname VARCHAR(32)," +
+                " balance DECIMAL," +
+                " creditlimit DECIMAL," +
+                " accountcreated  VARCHAR(12)," +
+                " rating VARCHAR(5)," +
+                " PRIMARY KEY (account));");
 
-    private static Connection getJdbcConnection() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-        Driver driver = (Driver) Class.forName("sun.jdbc.odbc.JdbcOdbcDriver").newInstance();
-        Properties properties = new Properties();
-        properties.put("user", "scott");
-        properties.put("password", "tiger");
-        return driver.connect("jdbc:odbc:dp-cookbook", properties);
+        DataWriter writer = new JdbcWriter(connection, "credit_balance");
+
+        Job.run(reader, writer);
     }
 }
