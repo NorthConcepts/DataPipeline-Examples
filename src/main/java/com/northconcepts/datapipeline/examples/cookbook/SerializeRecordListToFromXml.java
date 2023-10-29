4bb747eb31b390f7b531924e89c82d4ad6081ff2
sah2020 <akmaljonsamandarov@gmail.com>
2023-10-29 08:56:27 +0500

    DP-3612 Add example code for SerializeRecordListToFromXml

---
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/SerializeRecordListToFromXml.java
@@ -0,0 +1,29 @@
+package com.northconcepts.datapipeline.examples.cookbook;
+
+import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.core.RecordList;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class SerializeRecordListToFromXml {
+    public static void main(String[] args) throws IOException {
+        Record record1 = new Record();
+        record1.setField("name", "John Wayne");
+        record1.setField("balance", new BigInteger("1234567894"));
+
+        Record record2 = new Record();
+        record2.setField("name", "Peter Parker");
+        record2.setField("balance", new BigInteger("9876543210"));
+
+        RecordList recordList = new RecordList(record1, record2);
+
+        String xml = recordList.toRecord().toXml();
+        System.out.println("Serialization of record list to XML completed");
+        System.out.println(xml);
+
+        Record record = Record.fromXml(xml);
+        System.out.println("\nDeserialization of record list from XML completed");
+        System.out.println(record);
+    }
+}
