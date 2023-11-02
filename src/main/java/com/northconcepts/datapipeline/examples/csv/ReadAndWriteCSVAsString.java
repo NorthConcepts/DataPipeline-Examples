79feb417ad8f02448a200cb33f636612c9df0b65
Bryan-Noel <noelbryansoftware@gmail.com>
2023-11-02 15:47:19 +0800

    DP-3635

---
+++ src/main/java/com/northconcepts/datapipeline/examples/csv/ReadAndWriteCSVAsString.java
@@ -0,0 +1,17 @@
+package com.northconcepts.datapipeline.examples.csv;
+
+import com.northconcepts.datapipeline.csv.CommaSeparatedValues;
+
+public class ReadAndWriteCSVAsString {
+
+    private static final String[] CONTINENTS = {"Africa", "Antarctica", "Asia", "Australia", "Europe", "North America", "South America"};
+
+    public static void main(String[] args) {
+
+        CommaSeparatedValues commaSeparatedValues = CommaSeparatedValues.fromArray(CONTINENTS);
+        System.out.println(commaSeparatedValues.toCsvString());
+
+        commaSeparatedValues.remove(1);
+        System.out.println(commaSeparatedValues.toCsvString());
+    }
+}
