61cb1177e0fac6e64c001561a7357b269b3c2f27
w12025 <john0xsd@gmail.com>
2023-11-09 19:05:40 +0500

    DP-3637 Apply snake_case and varchar length

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/GenerateMySqlDdlProgrammatically.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/GenerateMySqlDdlProgrammatically.java
@@ -7,26 +7,26 @@ public class GenerateMySqlDdlProgrammatically {
     public static void main(String[] args) {
         MySqlPart createUsersTable = new CreateTable("employee")
                 .addColumn(new CreateTableColumn("id", TableColumnType.INT).setAutoIncrement(true).setPrimaryKey(true))
-                .addColumn(new CreateTableColumn("name", TableColumnType.VARCHAR).setNullable(false))
-                .addColumn(new CreateTableColumn("email", TableColumnType.VARCHAR).setUnique(true))
-                .addColumn(new CreateTableColumn("departmentId", TableColumnType.INT))
+                .addColumn(new CreateTableColumn("name", TableColumnType.VARCHAR).setNullable(false).setLength(100))
+                .addColumn(new CreateTableColumn("email", TableColumnType.VARCHAR).setUnique(true).setLength(50))
+                .addColumn(new CreateTableColumn("department_id", TableColumnType.INT))
                 .setPretty(true);
 
         SqlPart createAddressTable = new CreateTable("department")
                 .addColumn(new CreateTableColumn("id", TableColumnType.INT).setAutoIncrement(true).setPrimaryKey(true))
-                .addColumn(new CreateTableColumn("name", TableColumnType.VARCHAR))
-                .addColumn(new CreateTableColumn("description", TableColumnType.TEXT))
-                .addColumn(new CreateTableColumn("managerId", TableColumnType.INT))
+                .addColumn(new CreateTableColumn("name", TableColumnType.VARCHAR).setLength(100))
+                .addColumn(new CreateTableColumn("description", TableColumnType.VARCHAR).setLength(255))
+                .addColumn(new CreateTableColumn("manager_id", TableColumnType.INT))
                 .setPretty(true);
 
         SqlPart addDepartmentIdForeignKey = new CreateForeignKey("fk_employee_department", "department", "employee")
-                .setForeignKeyColumnNames("departmentId")
+                .setForeignKeyColumnNames("department_id")
                 .setPrimaryKeyColumnNames("id")
                 .setOnDeleteAction(ForeignKeyAction.SET_NULL)
                 .setPretty(true);
 
         SqlPart addManagerIdForeignKey = new CreateForeignKey("fk_department_manager", "employee", "department")
-                .setForeignKeyColumnNames("managerId")
+                .setForeignKeyColumnNames("manager_id")
                 .setPrimaryKeyColumnNames("id")
                 .setPretty(true);
 
