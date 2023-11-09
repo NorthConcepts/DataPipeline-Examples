87549d43c8f629d80e466f7356e233a7a22329d5
w12025 <john0xsd@gmail.com>
2023-11-09 19:01:39 +0500

    DP-3636 Apply snake_case and varchar length

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/GeneratePostgreSqlDdlProgrammatically.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/GeneratePostgreSqlDdlProgrammatically.java
@@ -7,21 +7,21 @@ public class GeneratePostgreSqlDdlProgrammatically {
     public static void main(String[] args) {
         SqlPart createUsersTable = new CreateTable("users")
                 .addColumn(new CreateTableColumn("id", TableColumnType.SERIAL).setPrimaryKey(true))
-                .addColumn(new CreateTableColumn("name", TableColumnType.VARCHAR).setNullable(false))
-                .addColumn(new CreateTableColumn("email", TableColumnType.VARCHAR).setUnique(true))
-                .addColumn(new CreateTableColumn("addressId", TableColumnType.INT))
+                .addColumn(new CreateTableColumn("name", TableColumnType.VARCHAR).setNullable(false).setLength(100))
+                .addColumn(new CreateTableColumn("email", TableColumnType.VARCHAR).setUnique(true).setLength(50))
+                .addColumn(new CreateTableColumn("address_id", TableColumnType.INT))
                 .setPretty(true);
 
         SqlPart createAddressTable = new CreateTable("address")
                 .addColumn(new CreateTableColumn("id", TableColumnType.SERIAL).setPrimaryKey(true))
-                .addColumn(new CreateTableColumn("address", TableColumnType.VARCHAR))
-                .addColumn(new CreateTableColumn("city", TableColumnType.VARCHAR))
-                .addColumn(new CreateTableColumn("state", TableColumnType.VARCHAR))
+                .addColumn(new CreateTableColumn("address", TableColumnType.VARCHAR).setLength(255))
+                .addColumn(new CreateTableColumn("city", TableColumnType.VARCHAR).setLength(50))
+                .addColumn(new CreateTableColumn("state", TableColumnType.VARCHAR).setLength(50))
                 .addColumn(new CreateTableColumn("country", TableColumnType.VARCHAR).setLength(3))
                 .setPretty(true);
 
         SqlPart addForeignKey = new CreateForeignKey("fk_users_address", "address", "users")
-                .setForeignKeyColumnNames("addressId")
+                .setForeignKeyColumnNames("address_id")
                 .setPrimaryKeyColumnNames("id")
                 .setOnDeleteAction(ForeignKeyAction.CASCADE)
                 .setPretty(true);
