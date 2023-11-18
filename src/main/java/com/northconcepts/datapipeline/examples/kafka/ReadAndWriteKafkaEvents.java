a24067c2486d80d06bf40959ed49b0f9e51fe6ea
Bryan Noel <42918023+Bryan-Noel@users.noreply.github.com>
2023-11-19 03:33:11 +0800

    DP-3642-Add-example-Kafka-code  (#191)
    * DP-3642
    
    * DP-3642
    
    * DP-3642: review updates
    
    * DP-3642

---
+++ src/main/java/com/northconcepts/datapipeline/examples/kafka/ReadAndWriteKafkaEvents.java
@@ -0,0 +1,40 @@
+package com.northconcepts.datapipeline.examples.kafka;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.csv.CSVWriter;
+import com.northconcepts.datapipeline.job.Job;
+import com.northconcepts.datapipeline.kafka.KafkaReader;
+import com.northconcepts.datapipeline.kafka.KafkaWriter;
+
+import java.io.File;
+import java.util.Properties;
+
+public class ReadAndWriteKafkaEvents {
+
+    private static String BOOTSTRAP_SERVERS = "localhost:9092,another.host:port";
+    private static String GROUP_ID = "group_id";
+    private static String TOPIC = "jewelry";
+
+    public static void main(String[] args) {
+        DataReader reader = new CSVReader(new File("data/input/jewelry.csv"))
+            .setAllowMultiLineText(true)
+            .setFieldNamesInFirstRow(true);
+
+        Properties writerProps = new Properties();
+        writerProps.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        DataWriter writer = new KafkaWriter(writerProps, TOPIC);
+
+        Job.runAsync(reader, writer);
+
+        Properties readerProps = new Properties();
+        readerProps.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        readerProps.put("group.id", GROUP_ID);
+
+        reader = new KafkaReader(readerProps, TOPIC, 5000L).setKeepPolling(false);
+        writer = new CSVWriter(new File("data/output/jewelry-events.csv"));
+
+        Job.run(reader, writer);
+    }
+}
