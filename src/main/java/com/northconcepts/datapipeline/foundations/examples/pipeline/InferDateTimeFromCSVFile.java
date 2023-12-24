c2e85efb64258f6e08dbfd5b5ff316ada649dae5
sah2020 <69834171+sah2020@users.noreply.github.com>
2023-12-24 19:54:08 +0500

    DP-3711 Add example for InferDateTimeFromCSVFile (#196)
    * DP-3711 Add example for InferDateTimeFromCSVFile
    
    * DP-3711 Change pipeline input to dataReader factory

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/pipeline/InferDateTimeFromCSVFile.java
@@ -0,0 +1,42 @@
+package com.northconcepts.datapipeline.foundations.examples.pipeline;
+
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.foundations.pipeline.Pipeline;
+import com.northconcepts.datapipeline.foundations.pipeline.dataset.Column;
+import com.northconcepts.datapipeline.foundations.pipeline.dataset.Dataset;
+import com.northconcepts.datapipeline.foundations.pipeline.dataset.MemoryDataset;
+import com.northconcepts.datapipeline.foundations.time.DateTimePattern;
+
+import java.io.File;
+import java.util.Map;
+import java.util.concurrent.atomic.LongAdder;
+
+public class InferDateTimeFromCSVFile {
+    public static void main(String[] args) {
+        Pipeline pipeline = new Pipeline();
+        pipeline.setInputAsDataReaderFactory(() -> new CSVReader(new File("example/data/input/DateTimeData.csv")).setFieldNamesInFirstRow(true));
+
+        Dataset dataset = new MemoryDataset(pipeline);
+        dataset.load().waitForColumnStatsToLoad();
+
+        System.out.println("Column Count: " + dataset.getColumnCount());
+        System.out.println("Record Count: " + dataset.getRecordCount());
+
+        for (Column column : dataset.getColumns()) {
+            System.out.println("===============================");
+            System.out.println("Name: " + column.getName());
+            System.out.println("Value Count: " + column.getValueCount());
+
+            System.out.println("Is Temporal Column: " + column.getTemporal());
+            System.out.println("    Inferred Temporal Value Count: " + column.getInferredTemporalValueCount());
+            if (column.getTemporal()) {
+                for (Map.Entry<DateTimePattern, LongAdder> entry : column.getTemporalPatterns().entrySet()) {
+                    System.out.println("    " + entry.getKey().getPattern() + "  --  " + entry.getValue().longValue());
+                }
+            }
+
+            System.out.println("Inferred Type Name: " + column.getInferredTypeName());
+            System.out.println("Inferred Field Type: " + column.getInferredFieldType());
+        }
+    }
+}
