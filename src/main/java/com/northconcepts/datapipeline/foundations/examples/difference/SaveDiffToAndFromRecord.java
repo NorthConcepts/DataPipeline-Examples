3347c4ce14ccd2dd6e51a849c45e68adf6bc079d
Bryan Noel <42918023+Bryan-Noel@users.noreply.github.com>
2024-02-13 20:07:13 +0800

    DP-3816: init commit (#200)

---
+++ src/main/java/com/northconcepts/datapipeline/foundations/examples/difference/SaveDiffToAndFromRecord.java
@@ -0,0 +1,45 @@
+package com.northconcepts.datapipeline.foundations.examples.difference;
+
+import com.amazonaws.thirdparty.joda.time.LocalDate;
+import com.northconcepts.datapipeline.core.FieldType;
+import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.foundations.difference.Diff;
+import com.northconcepts.datapipeline.foundations.difference.RecordDiff;
+import com.northconcepts.datapipeline.foundations.schema.EntityDef;
+import com.northconcepts.datapipeline.foundations.schema.NumericFieldDef;
+import com.northconcepts.datapipeline.foundations.schema.TextFieldDef;
+import com.northconcepts.datapipeline.foundations.schema.diff.EntityDefDiff;
+
+import java.math.BigDecimal;
+
+public class SaveDiffToAndFromRecord {
+
+    public static void main(String[] args) {
+        Record oldRecord = new Record()
+            .setField("name", "John Doe")
+            .setField("dob", LocalDate.parse("2000-01-01"))
+            .setField("languages", new String[] {"English", "French"})
+            .setField("height", 1.70)
+            .setField("netIncome", new BigDecimal(100_286.99));
+
+        Record newRecord = new Record()
+            .setField("name", "John Doe")
+            .setField("age", 24)
+            .setField("languages", new String[] {"English", "French", "Spanish"})
+            .setField("height", 1.73)
+            .setField("netIncome", new BigDecimal(120_286.99));
+
+        RecordDiff oldDiff = RecordDiff.diff("userRecord", oldRecord, newRecord, "height", "netIncome");
+        System.out.println("old diff: " + oldDiff);
+
+        System.out.println("===============================================================");
+
+        Record diffRecord = oldDiff.toRecord();
+        System.out.println(diffRecord);
+
+        System.out.println("===============================================================");
+
+        RecordDiff newDiff = new RecordDiff().fromRecord(diffRecord);
+        System.out.println("new diff: " + newDiff);
+    }
+}
