dba03ccf475de134ac21f420d45521d1a6ebcd6d
gvjoshi25 <gvjoshi25@gmail.com>
2024-07-12 21:53:21 -0400

    DP-4077 - How to read an xml file with byte order mark

---
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadAnXmlFileWithByteOrderMark.java
@@ -0,0 +1,57 @@
+package com.northconcepts.datapipeline.examples.cookbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.xml.XMLConstants;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+
+import org.apache.commons.io.ByteOrderMark;
+import org.apache.commons.io.input.BOMInputStream;
+import org.apache.log4j.Logger;
+
+import com.northconcepts.datapipeline.core.DataEndpoint;
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.internal.xpath.XmlNodeReader;
+import com.northconcepts.datapipeline.xml.XmlReader;
+
+public class ReadAnXmlFileWithByteOrderMark {
+    
+    public static final Logger log = DataEndpoint.log;
+
+    public static void main(String[] args) throws Throwable {
+        BOMInputStream bomInputStream = BOMInputStream.builder()
+                .setInputStream(new FileInputStream(new File("example/data/input/xml_with_byte_order_marks.xml")))
+                .setByteOrderMarks(ByteOrderMark.UTF_8, ByteOrderMark.UTF_16BE, ByteOrderMark.UTF_16LE,
+                        ByteOrderMark.UTF_32BE, ByteOrderMark.UTF_32LE)
+                .setInclude(false)
+                .get();
+        
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        factory.setProperty(XMLInputFactory.IS_VALIDATING, false);
+        factory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+        factory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+        factory.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+        
+        XMLStreamReader streamReader = factory.createXMLStreamReader(bomInputStream);
+        DataReader reader = new XmlReader(new XmlNodeReader(streamReader))
+        	.addField("title", "//book/title/text()")
+        	.addField("language", "//book/title/@lang")
+        	.addField("price", "//book/price/text()")
+        	.addRecordBreak("//book");
+
+        reader.open();
+        try {
+            Record record;
+            while ((record = reader.read()) != null) {
+                log.info(record);
+                System.out.println(record);
+            }
+        } finally {
+            reader.close();
+        }
+    }
+
+}
