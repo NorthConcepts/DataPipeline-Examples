154db7542c4882f291e99b0163bbbf49f2ac1ea2
gvjoshi25 <gvjoshi25@gmail.com>
2024-07-19 17:03:10 -0400

    removed unnecessary complexity in code

--- src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadAnXmlFileWithByteOrderMark.java
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadAnXmlFileWithByteOrderMark.java
@@ -2,49 +2,40 @@ package com.northconcepts.datapipeline.examples.cookbook;
 
 import java.io.File;
 import java.io.FileInputStream;
-
-import javax.xml.stream.XMLStreamReader;
+import java.io.InputStreamReader;
 
 import org.apache.commons.io.ByteOrderMark;
 import org.apache.commons.io.input.BOMInputStream;
-import org.apache.log4j.Logger;
 
-import com.northconcepts.datapipeline.core.DataEndpoint;
 import com.northconcepts.datapipeline.core.DataReader;
-import com.northconcepts.datapipeline.core.Record;
-import com.northconcepts.datapipeline.internal.lang.Util;
-import com.northconcepts.datapipeline.internal.xpath.XmlNodeReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.job.Job;
 import com.northconcepts.datapipeline.xml.XmlReader;
 
 public class ReadAnXmlFileWithByteOrderMark {
     
-    public static final Logger log = DataEndpoint.log;
-
     public static void main(String[] args) throws Throwable {
         BOMInputStream bomInputStream = BOMInputStream.builder()
                 .setInputStream(new FileInputStream(new File("example/data/input/xml_with_byte_order_marks.xml")))
-                .setByteOrderMarks(ByteOrderMark.UTF_8, ByteOrderMark.UTF_16BE, ByteOrderMark.UTF_16LE,
-                        ByteOrderMark.UTF_32BE, ByteOrderMark.UTF_32LE)
+                .setByteOrderMarks(
+                        ByteOrderMark.UTF_8,
+                        ByteOrderMark.UTF_16BE,
+                        ByteOrderMark.UTF_16LE,
+                        ByteOrderMark.UTF_32BE,
+                        ByteOrderMark.UTF_32LE
+                        )
                 .setInclude(false)
                 .get();
         
-        XMLStreamReader streamReader = Util.newXMLInputFactory().createXMLStreamReader(bomInputStream);
-        DataReader reader = new XmlReader(new XmlNodeReader(streamReader))
+        DataReader reader = new XmlReader(new InputStreamReader(bomInputStream))
         	.addField("title", "//book/title/text()")
         	.addField("language", "//book/title/@lang")
         	.addField("price", "//book/price/text()")
         	.addRecordBreak("//book");
 
-        reader.open();
-        try {
-            Record record;
-            while ((record = reader.read()) != null) {
-                log.info(record);
-                System.out.println(record);
-            }
-        } finally {
-            reader.close();
-        }
+        DataWriter writer = StreamWriter.newSystemOutWriter();
+        Job.run(reader, writer);
     }
 
 }
