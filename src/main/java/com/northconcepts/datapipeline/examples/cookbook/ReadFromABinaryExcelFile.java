9988da86960f882b5a2f5f62af03ced9717e8f89
gvjoshi25 <gvjoshi25@gmail.com>
2025-03-11 19:00:17 -0400

    DP-4740 - Read a binary Excel file (.xlsb)

---
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/ReadFromABinaryExcelFile.java
@@ -0,0 +1,25 @@
+package com.northconcepts.datapipeline.examples.cookbook;
+
+import java.io.File;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.excel.ExcelDocument;
+import com.northconcepts.datapipeline.excel.ExcelDocument.ProviderType;
+import com.northconcepts.datapipeline.excel.ExcelReader;
+import com.northconcepts.datapipeline.job.Job;
+
+public class ReadFromABinaryExcelFile {
+
+    public static void main(String[] args) throws Throwable {
+        ExcelDocument document = new ExcelDocument(ProviderType.POI_XSSFB)
+                .open(new File("example/data/input/employee_details.xlsb"));
+        DataReader reader = new ExcelReader(document)
+                                  //.setSheetName("employee_details")
+                                  .setFieldNamesInFirstRow(true)
+                                  .setAutoCloseDocument(true);
+
+        
+        Job.run(reader, StreamWriter.newSystemOutWriter());
+    }
+}
