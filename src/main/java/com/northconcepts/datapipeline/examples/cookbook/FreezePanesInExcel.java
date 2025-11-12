120179edeb29f18c047164def5b01c663c626392
Gautam Joshi <gvjoshi25@gmail.com>
2025-11-12 18:49:40 -0500

    DP-5314 - Freeze panes in Excel (#226)
    * DP-5314 - Freeze panes in Excel
    
    * renamed java class

---
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/FreezePanesInExcel.java
@@ -0,0 +1,33 @@
+package com.northconcepts.datapipeline.examples.cookbook;
+
+import java.io.File;
+
+import com.northconcepts.datapipeline.core.DataEndpoint;
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.excel.ExcelDocument;
+import com.northconcepts.datapipeline.excel.ExcelWriter;
+import com.northconcepts.datapipeline.job.Job;
+
+import org.apache.log4j.Logger;
+
+public class FreezePanesInExcel {
+
+    public static final Logger log = DataEndpoint.log;
+
+    public static void main(String[] args) {
+        DataReader reader = new CSVReader(new File("example/data/input/patient-visits-raw-10000.csv"))
+                .setFieldNamesInFirstRow(true);
+
+        ExcelDocument document = new ExcelDocument();
+        DataWriter writer = new ExcelWriter(document).setSheetName("balance")
+                .setFreezeRows(1) // freeze pane is applied to the first row only.
+                .setFreezeColumns(2); // freeze pane is applied to the first two columns.
+
+        Job.run(reader, writer);
+        
+        document.save(new File("example/data/output/patient-visits-with-freeze-pane.xlsx"));
+    }
+
+}
