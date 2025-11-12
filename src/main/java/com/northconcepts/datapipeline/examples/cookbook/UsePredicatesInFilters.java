52b1973a4611239054a4a1f6fb7e0b9c2bffbb1f
Yafet Berhanu <yafetberhanu3@gmail.com>
2025-11-12 17:38:51 +0300

    DP-5337-Create example on how to use predicates in filters

---
+++ src/main/java/com/northconcepts/datapipeline/examples/cookbook/UsePredicatesInFilters.java
@@ -0,0 +1,31 @@
+package com.northconcepts.datapipeline.examples.cookbook;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.core.Record;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.csv.CSVReader;
+import com.northconcepts.datapipeline.filter.Filter;
+import com.northconcepts.datapipeline.filter.FilteringReader;
+import com.northconcepts.datapipeline.job.Job;
+
+import java.io.File;
+import java.util.function.Predicate;
+
+public class UsePredicatesInFilters {
+
+    public static void main(String[] args) {
+        DataReader reader = new CSVReader(new File("example/data/input/credit-balance-01.csv"))
+                .setFieldNamesInFirstRow(true);
+
+        Predicate<Record> aRatingPredicate = record -> record.getField("Rating").getValue().equals("A");
+        Filter aRatingFilter = Filter.of(aRatingPredicate);
+
+        FilteringReader filteringReader = new FilteringReader(reader);
+        filteringReader.add(aRatingFilter);
+
+        DataWriter writer = new StreamWriter(System.out);
+
+        Job.run(filteringReader, writer);
+    }
+}
